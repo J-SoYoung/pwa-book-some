13caad00d3bbd9eb1a3c1c5dacb8b7f2e79baf7c
thdud <thgml1444@naver.com>
2024-12-10 00:59:45 +0900

    Add PWA설치 버튼과 PWA설치 여부를 판단하는 코드 추가 - window.matchMedia로 PWA의 설치 유무 확인 ( display-mode가 독립적인지`standalone` 확인 후 boolean값 반환 - 이미 PWA가 설치돼있다면 Home페이지로 이동 - 없으면 install버튼을 클릭할 수 있게 화면 보여줌

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import PWABadge from "./PWABadge.tsx";
 import "./App.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import PWABadge from "./PWABadge.tsx"; // 서비스워커 업데이트
+import { InstallButton } from "./components/InstallButton.tsx"; // pwa로 설치
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // PWA가 설치되어 있는지 확인
+    const isStandalone = window.matchMedia(
+      "(display-mode: standalone)"
+    ).matches;
+
+    console.log(window.matchMedia("(display-mode: standalone)"));
+
+    if (isStandalone) {
+      // 설치된 상태라면 Home으로 리디렉션
+      navigate("/home");
+    }
+  }, [navigate]);
 
   return (
     <>
-      <h1>BOOK-SOME</h1>
+      <h1>BOOK-SOME landing</h1>
+      <InstallButton />
       <PWABadge />
     </>
   );

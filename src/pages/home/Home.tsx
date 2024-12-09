07eac30d117f80407be2b9fc081bc638845344e1
thdud <thgml1444@naver.com>
2024-12-09 14:59:58 +0900

    Styling global.css and reactive styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,9 @@
-export const Home = () =>{
-  return (<div>Home</div>)
-}
+import styles from "./home.module.css";
+
+export const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Booksome</h1>
+    </div>
+  );
+};

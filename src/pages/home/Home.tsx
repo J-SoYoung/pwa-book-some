990f48046a7ee3e13d8b0fd65fb733d79d28a2ce
thdud <thgml1444@naver.com>
2024-12-10 11:36:28 +0900

    Add page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 
 export const Home = () => {
   return (
     <div className={styles.container}>
-      <h1>Booksome</h1>
+      <h1>Booksome-home</h1>
+      <div className={styles.bottomNavbar}>
+        <Link to='/home'>HOME</Link>
+        <Link to='/mybook'>MY BOOK</Link>
+        <Link to='/posts'>글작성</Link>
+      </div>
     </div>
   );
 };

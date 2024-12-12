5dc3953ab8cfade75cc48b6b10e99c7c18e0d281
thdud <thgml1444@naver.com>
2024-12-12 14:03:09 +0900

    Update, Add : router의 App컴포넌트 대신 Landing으로 변경 및 공통 Layout설정

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,38 +1,50 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "../App";
-import '../index.css'
+import "../index.css";
 
+import Layout from "../components/Layout";
 import Home from "@/pages/home/";
 import Login from "@/pages/login";
 import MyBook from "@/pages/my-book";
 import Posts from "@/pages/posts";
 import Detail from "@/pages/detail";
+import PostsNew from "@/pages/postsNew";
+import Landing from "@/pages/Landing";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
-  },
-  {
-    path: "/home",
-    element: <Home />
-  },
-  {
-    path: "/detail",
-    element: <Detail />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/mybook",
-    element: <MyBook />
-  },
-  {
-    path: "/posts",
-    element: <Posts />
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing /> // 랜딩페이지
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+      {
+        path: "/detail",
+        element: <Detail />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/mybook",
+        element: <MyBook />
+      },
+      {
+        path: "/postsNew",
+        element: <PostsNew />
+      },
+      {
+        path: "/posts",
+        element: <Posts />
+      }
+    ]
+  }
 ]);
 
 export default function Router() {

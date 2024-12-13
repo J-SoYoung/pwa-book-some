e4ebc9cd92c9b735b336b56595be9c47f24a67e9
thdud <thgml1444@naver.com>
2024-12-13 16:45:05 +0900

    Update : 포스팅 작성 페이지로 이동시 팝업이 닫히게 Link태그에도 onClose함수를 연결해줌

--- src/components/popupButton/PopupButton.tsx
+++ src/components/popupButton/PopupButton.tsx
@@ -2,14 +2,17 @@ import styles from "./popupButton.module.css";
 import { Link } from "react-router-dom";
 
 function PopupButton({ onClose }: { onClose: () => void }) {
+  const handleLinkClick = () =>{
+    onClose()
+  }
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
         <button onClick={onClose} className={styles.closeButton}>
           ✕
         </button>
-        <Link to={'/postsNew'} className={styles.diaryButton}>새 다이어리 생성</Link>
-        <Link to={'/posts'} className={styles.diaryButton}>기존 다이어리 작성</Link>
+        <Link to={'/postsNew'} className={styles.diaryButton}  onClick={handleLinkClick}>새 다이어리 생성</Link>
+        <Link to={'/posts'} className={styles.diaryButton}  onClick={handleLinkClick}>기존 다이어리 작성</Link>
       </div>
     </div>
   );

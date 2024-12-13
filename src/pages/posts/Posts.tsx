f0406c11970a8c851cd2dd1597c45cf016e01d95
thdud <thgml1444@naver.com>
2024-12-13 17:17:35 +0900

    Add : 포스팅 페이지 추가

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,3 +1,41 @@
+import styles from "./post.module.css";
 export const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <div className={styles.posts}>
+      <h2>BookDiary 작성</h2>
+
+      <form className={styles.form}>
+        <label className={styles.label}>
+          다이어리 검색
+          <div className={styles.selectWrapper}>
+            <select className={styles.select}>
+              <option value="" disabled selected>
+                다이어리를 선택해주세요
+              </option>
+              <option value="diary1">다이어리 1</option>
+              <option value="diary2">다이어리 2</option>
+              <option value="diary3">다이어리 3</option>
+            </select>
+          </div>
+        </label>
+        <label className={styles.label}>
+          오늘의 독서 제목을 적어주세요.
+          <input
+            type="text"
+            placeholder="오늘 작성된 감상평의 제목이 됩니다."
+            className={styles.input}
+          />
+        </label>
+
+        <label className={styles.label}>
+          오늘 읽은 부분을 기록해보세요.
+          <textarea placeholder="" className={styles.textarea}></textarea>
+        </label>
+
+        <button type="submit" className={styles.submitButton}>
+          글작성
+        </button>
+      </form>
+    </div>
+  );
 };
--- src/pages/postsNew/PostsNew.tsx
+++ src/pages/postsNew/PostsNew.tsx
@@ -1,3 +1,49 @@
+import styles from "./postNew.module.css";
 export const PostsNew = () => {
-  return <div>PostsNew</div>;
+  return (
+    <div className={styles.posts}>
+      <h2>새 다이어리 만들기</h2>
+
+      <div className={styles.searchSection}>
+        <button className={styles.searchButton}>
+          <span role="img" aria-label="search">
+            책
+          </span>
+        </button>
+        <div className={styles.bookInfo}>
+          <img src={"/"} className={styles.bookImage} />
+          <div className={styles.bookDetails}></div>
+        </div>
+      </div>
+
+      <form className={styles.form}>
+        <label className={styles.label}>
+          나만의 책 제목을 적어주세요.
+          <input
+            type="text"
+            placeholder="책 다이어리 제목이 됩니다"
+            className={styles.input}
+          />
+        </label>
+
+        <label className={styles.label}>
+          오늘의 독서 제목을 적어주세요.
+          <input
+            type="text"
+            placeholder="오늘 작성된 감상평의 제목이 됩니다."
+            className={styles.input}
+          />
+        </label>
+
+        <label className={styles.label}>
+          오늘 읽은 부분을 기록해보세요.
+          <textarea placeholder="" className={styles.textarea}></textarea>
+        </label>
+
+        <button type="submit" className={styles.submitButton}>
+          글작성
+        </button>
+      </form>
+    </div>
+  );
 };

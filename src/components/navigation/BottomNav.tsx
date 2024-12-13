4951f77e0217101added68f840a985f9dfff9cea
thdud <thgml1444@naver.com>
2024-12-13 16:24:39 +0900

    간단한 수정

--- src/components/navigation/BottomNav.tsx
+++ src/components/navigation/BottomNav.tsx
@@ -11,9 +11,9 @@ function BottomNav() {
   return (
     <>
       <nav className={styles.bottomNav}>
-        <Link to="/home">HOME</Link>
-        <button onClick={togglePopup}>+ 글쓰기</button>
-        <Link to="/mybook">내 서재</Link>
+        <Link to="/home" className={styles.bottomNavItems}>HOME</Link>
+        <button onClick={togglePopup} className={styles.bottomNavItems}>+ 글쓰기</button>
+        <Link to="/mybook" className={styles.bottomNavItems}>내 서재</Link>
       </nav>
       {showPopup && <PopupButton onClose={togglePopup} />}
     </>
--- src/pages/home/recommendbooks/RecommedBooks.tsx
+++ src/pages/home/recommendbooks/RecommedBooks.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import styles from "./recommendations.module.css";
 
 function Recommendations() {
@@ -6,9 +6,15 @@ function Recommendations() {
     <section className={styles.recommendations}>
       <h2>이 책을 추천합니다</h2>
       <div className={styles.bookGrid}>
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
+        <Link to={"/detail"}>
+          <div className={styles.bookItem}></div>
+        </Link>
+        <Link to={"/detail"}>
+          <div className={styles.bookItem}></div>
+        </Link>
+        <Link to={"/detail"}>
+          <div className={styles.bookItem}></div>
+        </Link>
       </div>
     </section>
   );

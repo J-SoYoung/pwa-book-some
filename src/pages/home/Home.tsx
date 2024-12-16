a209ae288c92f19896141bbe5ebbcd9918dcc88f
thdud <thgml1444@naver.com>
2024-12-16 10:57:10 +0900

    사용하지 않는 코드 제거

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,17 @@
-import styles from './home.module.css'
+import { useEffect } from "react";
+import styles from "./home.module.css";
+
 import SearchBar from "@/components/searchBar/SearchBar";
 import Recommendations from "./recommendbooks/RecommedBooks";
 import UserBooks from "./userBooks/UserBooks";
+import { searchBooks } from "@/apis/book";
 
 export const Home = () => {
+
+  useEffect(() => {
+    searchBooks();
+  }, []);
+
   return (
     <main className={styles.home}>
       <SearchBar />

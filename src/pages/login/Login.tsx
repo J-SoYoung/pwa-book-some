c00eb46858d41590083d9ebaac26cbb5a5b67f91
thdud <thgml1444@naver.com>
2024-12-16 12:50:13 +0900

    Add 로그인 페이지의 onClickLogin 함수 생성, firebase의 google login함수와 연결

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,32 @@
+import { useEffect } from "react";
 import styles from "./login.module.css";
 import { FcGoogle } from "react-icons/fc";
+import { firebaseGoogleLogin } from "@/services/auth";
 
 export const Login = () => {
+  useEffect(() => {
+    // recoil에 유저 데이터 있는지 확인
+    // 있으면 로그아웃 버튼 view
+  }, []);
+
+  const onClickLogin = async () => {
+    try {
+      const googleLogin = await firebaseGoogleLogin();
+      console.log(googleLogin);
+    } catch (error) {
+      console.error("로그인 실패:", error);
+    }
+  };
+
   return (
     <div className={styles.logoContainer}>
       <div className={styles.logoCircle}>
         <p>BOOKSOME</p>
         <img src="/" alt="logo" />
       </div>
-      <div className={styles.loginButton}>
-        <FcGoogle className={styles.googleLogo}/>
+      <div className={styles.loginButton} onClick={onClickLogin}>
+        <FcGoogle className={styles.googleLogo} />
+        <span>로그인</span>
       </div>
     </div>
   );

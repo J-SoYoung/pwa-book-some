d578fa24034027037e42cc75737400c774b70bfa
thdud <thgml1444@naver.com>
2024-12-18 18:27:54 +0900

    Test : recoil이 유저 데이터를 잘 가져왔는지 확인하기 위한 TEST, 아직 UI의 완성이 안되었으므로 임시로 둠

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
 import styles from "./home.module.css";
+import { useRecoilValue } from "recoil";
 
+import { userState } from "@/recoil/atoms";
 import SearchBar from "@/components/searchBar/SearchBar";
 import Recommendations from "./recommendbooks/RecommedBooks";
 import UserBooks from "./userBooks/UserBooks";
-import { searchBooks } from "@/apis/book";
 
 export const Home = () => {
-  useEffect(() => {
-    console.log("미리보기중");
-    searchBooks();
-  }, []);
+  const user = useRecoilValue(userState);
+  
 
   return (
     <main className={styles.home}>
       <SearchBar />
+      <p>{user.username}님</p>
       <Recommendations />
       <UserBooks />
     </main>

ccececa3be3e24b77b2b5758e2ae5b63a1155ad1
thdud <thgml1444@naver.com>
2024-12-18 18:25:02 +0900

    Add : firebase의 소셜로그인 구현, 로그인 유저 여부에 따라 로그인 페이지의 문구 변경 - signInFormGoogle()함수를 호출하여 firebase의 googleLogin메서드를 사용하도록 구현 - firebase에서 유저 정보를 받아와 serUserData에 업데이트 하여 recoil이 user데이터를 관리하도록 함 - signOutFromGoogle()함수를 호출하여 firebase의 로그아웃 메서드를 사용하여 로그아웃 구현 - 로그아웃 함수 실행시 recoil에 있는 유저 데이터도  초기화시킴 - recoil이 관리하고 있는 userData여부에 따라 로그인 페이지의 문구 변경

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,49 @@
 import { useEffect } from "react";
 import styles from "./login.module.css";
 import { FcGoogle } from "react-icons/fc";
-import { firebaseGoogleLogin } from "@/services/auth";
+import { signInFormGoogle, signOutFromGoogle } from "@/services/auth";
+import { useRecoilState } from "recoil";
+import { userState } from "@/recoil/atoms";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  useEffect(() => {
-    // recoil에 유저 데이터 있는지 확인
-    // 있으면 로그아웃 버튼 view
-  }, []);
+  const navigate = useNavigate();
+  const [userData, setUserData] = useRecoilState(userState);
+  console.log(userData);
 
-  const onClickLogin = async () => {
+  // useEffect(() => {
+  //   const checkLoginUser = async () => {
+  //     const loginUser = await firebaseLogin();
+  //     setUserData(loginUser);
+  //   };
+  //   if (userData) {
+  //     navigate("/home");
+  //   }
+  //   checkLoginUser();
+
+  //   return () => {
+  //     checkLoginUser();
+  //   };
+  //   // recoil에 유저 데이터 있는지 확인
+  //   // 있으면 로그아웃 버튼 view
+  // }, [navigate, userData, setUserData]);
+
+  const onClickGoogleSignIn = async () => {
     try {
-      const googleLogin = await firebaseGoogleLogin();
-      console.log(googleLogin);
+      const signInUser = await signInFormGoogle();
+      setUserData(signInUser);
     } catch (error) {
       console.error("로그인 실패:", error);
+    }
+  };
+  const onClickSignOut = async() => {
+    if (confirm('정말 로그아웃 하시겠습니까?>')) {
+      try {
+        await signOutFromGoogle();
+        setUserData('');
+      } catch (error) {
+        console.error('로그아웃 실패:', error);
+      }
     }
   };
 
@@ -24,10 +53,17 @@ export const Login = () => {
         <p>BOOKSOME</p>
         <img src="/" alt="logo" />
       </div>
-      <div className={styles.loginButton} onClick={onClickLogin}>
-        <FcGoogle className={styles.googleLogo} />
-        <span>로그인</span>
-      </div>
+      {userData ? (
+        <div className={styles.loginButton} onClick={onClickSignOut}>
+          <FcGoogle className={styles.googleLogo} />
+          <span>Sign Out</span>
+        </div>
+      ) : (
+        <div className={styles.loginButton} onClick={onClickGoogleSignIn}>
+          <FcGoogle className={styles.googleLogo} />
+          <span>Sign In</span>
+        </div>
+      )}
     </div>
   );
 };

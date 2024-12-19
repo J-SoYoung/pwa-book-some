23d153bdc2c10d6891dbbf4eda00e5d03e731618
thdud <thgml1444@naver.com>
2024-12-19 13:24:22 +0900

    Updage : 사용하지 않는 코드 제거 및 recoil유저데이터 타입 지정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,32 +1,15 @@
-import { useEffect } from "react";
-import styles from "./login.module.css";
-import { FcGoogle } from "react-icons/fc";
-import { signInFormGoogle, signOutFromGoogle } from "@/services/auth";
-import { useRecoilState } from "recoil";
-import { userState } from "@/recoil/atoms";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { FcGoogle } from "react-icons/fc";
+
+import styles from "./login.module.css";
+import { userState } from "@/recoil/atoms";
+import { signInFormGoogle, signOutFromGoogle } from "@/services/auth";
 
 export const Login = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useRecoilState(userState);
   console.log(userData);
-
-  // useEffect(() => {
-  //   const checkLoginUser = async () => {
-  //     const loginUser = await firebaseLogin();
-  //     setUserData(loginUser);
-  //   };
-  //   if (userData) {
-  //     navigate("/home");
-  //   }
-  //   checkLoginUser();
-
-  //   return () => {
-  //     checkLoginUser();
-  //   };
-  //   // recoil에 유저 데이터 있는지 확인
-  //   // 있으면 로그아웃 버튼 view
-  // }, [navigate, userData, setUserData]);
 
   const onClickGoogleSignIn = async () => {
     try {
@@ -36,13 +19,14 @@ export const Login = () => {
       console.error("로그인 실패:", error);
     }
   };
-  const onClickSignOut = async() => {
-    if (confirm('정말 로그아웃 하시겠습니까?>')) {
+  const onClickSignOut = async () => {
+    if (confirm("정말 로그아웃 하시겠습니까?>")) {
       try {
         await signOutFromGoogle();
-        setUserData('');
+        setUserData(null);
+        navigate("/home");
       } catch (error) {
-        console.error('로그아웃 실패:', error);
+        console.error("로그아웃 실패:", error);
       }
     }
   };

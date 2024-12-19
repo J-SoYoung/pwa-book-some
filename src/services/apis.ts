fcfdf5c4a82ff7917183c96439b2fcea400a9c93
thdud <thgml1444@naver.com>
2024-12-19 13:30:16 +0900

    Add : 다이어리 작성시 해당 book이 db에 있다면 다이어리만 db에 추가하는 기능 추가, api함수 인자로 받아온 데이터 타입 지정

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -6,32 +6,38 @@
 //   }
 // };
 
-import { get, push, ref, set } from "firebase/database";
+import { get, ref, set, update } from "firebase/database";
 import { database } from "./firebase";
+import { NewDiaryDataType } from "@/types";
 
-export const uploadDiaryPosting = async (newDiaryData) => {
+export const uploadDiaryPosting = async (newDiaryData: NewDiaryDataType) => {
   try {
     const { books, diaries, posts, user } = newDiaryData;
 
     // 책 정보 저장
-    const bookRef = ref(database, `books/${books.id}`);
+    const bookRef = ref(database, `books/${books.isbn}`);
     const bookSnapshot = await get(bookRef);
     if (!bookSnapshot.exists()) {
       await set(bookRef, {
-        id: books.id,
+        id: books.isbn,
         title: books.title,
         image: books.image,
         author: books.author,
         description: books.description,
-        createdAt: books.createdAt,
-        diaries: diaries.id
+        diaries: { [diaries.id]: true }
       });
+    } else {
+      // 책은 존재한다면 다이어리 내용만 추가로 저장
+      const bookData = bookSnapshot.val();
+      const updatedDiaries = bookData.diaries || {};
+      updatedDiaries[diaries.id] = true;
+      await update(bookRef, { diaries: updatedDiaries });
     }
 
     // 다이어리 저장 (내가 저장한 id경로로 바로 저장)
     const diaryRef = ref(database, `diaries/${user.userId}/${diaries.id}`);
     await set(diaryRef, {
-      bookId: books.id,
+      bookId: books.isbn,
       title: diaries.title,
       createdAt: diaries.createdAt
     });

b8359439d0b1f57d6ef54dcbed3a80fff9538ecf
thdud <thgml1444@naver.com>
2024-12-21 23:15:19 +0900

    Add : home의 다이어리 가져오는 기능 구현 ( 다이어리 + 포스트 ) - 다이어리에는 book의 정보가 최소한으로 들어있다. - 다이어리와 포스트는 db에 따로 저장돼 있지만 각각의 id로 참조하고 있기 때문에 해당id를 활용해 데이터를 가져올 수 있다. - posts는 diaryid에 해당하는 데이터를 값으로 가져와 데이터를 반환한다

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -8,8 +8,14 @@
 
 import { get, ref, set, update } from "firebase/database";
 import { database } from "./firebase";
-import { NewDiaryDataType } from "@/types";
+import {
+  DiariesType,
+  DiariesWithPostsType,
+  NewDiaryDataType,
+  PostsType
+} from "@/types";
 
+// 다이어리 작성
 export const uploadDiaryPosting = async (newDiaryData: NewDiaryDataType) => {
   try {
     const { books, diaries, posts, user } = newDiaryData;
@@ -38,6 +44,9 @@ export const uploadDiaryPosting = async (newDiaryData: NewDiaryDataType) => {
     const diaryRef = ref(database, `diaries/${user.userId}/${diaries.id}`);
     await set(diaryRef, {
       bookId: books.isbn,
+      bookImage: books.image,
+      bookTitle: books.title,
+      id: diaries.id,
       title: diaries.title,
       createdAt: diaries.createdAt
     });
@@ -49,7 +58,50 @@ export const uploadDiaryPosting = async (newDiaryData: NewDiaryDataType) => {
       content: posts.content,
       createdAt: posts.createdAt
     });
+    return "포스트가 정상적으로 업로드 되었습니다";
   } catch (error) {
     console.error("uploadDiaryPosting 에러 --", error);
+  }
+};
+
+// 다이어리 가져오기
+export const getBookDiaries = async () => {
+  try {
+    // 다이어리 전체 가져오기
+    const diaryRef = ref(database, "diaries");
+    const diarySnapshot = await get(diaryRef);
+    if (!diarySnapshot.exists()) {
+      console.error("No diaries found.");
+      return [];
+    }
+    // 다이어리 가져오기
+    const diaryData = diarySnapshot.val();
+    const allDiaries: DiariesType[] = Object.values(diaryData).flatMap(
+      (userDiaries) => Object.values(userDiaries as DiariesType)
+    );
+
+    // 포스트 데이터 가져오기
+    const postsRef = ref(database, "posts");
+    const postsSnapshot = await get(postsRef);
+    if (!postsSnapshot.exists()) {
+      console.error("No posts found.");
+      return [];
+    }
+    const postsData = postsSnapshot.val();
+
+    // 다이어리와 포스트트 데이터 결합
+    const diariesWithPosts = allDiaries.map((diary): DiariesWithPostsType => {
+      const diaryPosts = postsData[String(diary.id)] || {};
+      return {
+        diaryTitle: diary.title,
+        bookImage: diary.bookImage as string,
+        bookTitle: diary.bookTitle as string,
+        posts: Object.values(diaryPosts) as PostsType[]
+      };
+    });
+    return diariesWithPosts;
+  } catch (error) {
+    console.error("다이어리 가져오기 에러", error);
+    return [];
   }
 };

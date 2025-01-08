a23d53b8f22472b55c8e8371484950a9f1d5ee41
thdud <thgml1444@naver.com>
2025-01-08 13:32:24 +0900

    Update : 클라이언트에서 naver API에 직접적으로 호출하는 코드 제거 -> vercel의 serverless함수에 호출함

--- src/bookApis/book.ts
+++ src/bookApis/book.ts
@@ -1,25 +1,38 @@
-export const searchBooks = async (query: string) => {
-  console.log("검색쿼리 api통신--", query);
-  try {
-    const response = await fetch(
-      `/search/book?query=${encodeURI(query)}&display=10&start=1&sort=sim`,
-      // 'https://openapi.naver.com/v1/search/blog?query=' + encodeURI(req.query.query);
-      {
-        method: "GET",
-        headers: {
-          "X-Naver-Client-Id": import.meta.env.VITE_APP_NAVER_CLIENT_ID,
-          "X-Naver-Client-Secret": import.meta.env.VITE_APP_NAVER_CLIENT_SECRET
-        }
-      }
-    );
+// export const searchBooks = async (query: string) => {
+//   console.log("검색쿼리 api통신--", query);
+//   try {
+//     const response = await fetch(
+//       `/search/book?query=${encodeURI(query)}&display=10&start=1&sort=sim`,
+//       // 'https://openapi.naver.com/v1/search/blog?query=' + encodeURI(req.query.query);
+//       {
+//         method: "GET",
+//         headers: {
+//           "X-Naver-Client-Id": import.meta.env.VITE_APP_NAVER_CLIENT_ID,
+//           "X-Naver-Client-Secret": import.meta.env.VITE_APP_NAVER_CLIENT_SECRET
+//         }
+//       }
+//     );
 
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     console.log("데이터 검색결과", data.items);
+//     return await data.items;
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// };
+
+
+export const searchBooks = async (query: string) => {
+  try {
+    const response = await fetch(`/api/searchBooks?query=${encodeURI(query)}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     const data = await response.json();
-    console.log("데이터 검색결과", data.items);
-    return await data.items;
+    return data;
   } catch (error) {
     console.error("Error:", error);
   }

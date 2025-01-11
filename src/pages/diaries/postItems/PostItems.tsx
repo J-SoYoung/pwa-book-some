59777eb26424f5e66aa395a7bfde4e6d21ba9af0
thdud <thgml1444@naver.com>
2025-01-11 14:03:06 +0900

    Add : diary페이지의 포스트 컴포넌트 생성

---
+++ src/pages/diaries/postItems/PostItems.tsx
@@ -0,0 +1,18 @@
+import { PostsType } from "@/types";
+import styles from "../diaries.module.css";
+
+export const PostItems = ({ posts }: { posts: PostsType[] }) => {
+  return (
+    <>
+      {posts.map((post) => (
+        <div className={styles.postsItems} key={post.id}>
+          <div className={styles.thumbnail}></div>
+          <div className={styles.details}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};

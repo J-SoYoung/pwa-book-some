a7c18d6de29853a04e930ae0fbdbc289f6d316a7
thdud <thgml1444@naver.com>
2025-01-14 19:05:13 +0900

    Update : dummydata를 삭제, db에서 가져온 데이터를 UI에 렌더링하였다. book 데이터는 그대로 두고, diary를 컴포넌트로 생성

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,59 +1,48 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styles from "./detail.module.css";
+import { DiarySection } from "./diarySection/DiarySection";
+import { useParams } from "react-router-dom";
+import { BookType, DiariesType, PostsType } from "@/services/types";
+import { getBookAndDiaries } from "@/services/apis";
 
-const books = {
-  image: "url_to_image",
-  title: "Book Title 1",
-  author: "작가명",
-  description:
-    "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.",
-  readers: ["userId1"]
-};
+export type DiaryAndPostsType = DiariesType & { firstPost: PostsType };
+export interface DetailDataType {
+  book: BookType;
+  diaries: DiaryAndPostsType[];
+}
 
 export const Detail = () => {
+  const { bookId } = useParams<{ bookId: string }>();
+  const [bookData, setBookData] = useState<BookType | null>(null);
+  const [diaries, setDiaries] = useState<DiaryAndPostsType[] | []>([]);
+
+  useEffect(() => {
+    const fetchBookData = async () => {
+      const response = await getBookAndDiaries(bookId as string);
+      if (response) {
+        const { book, diaries }: DetailDataType = response;
+        setBookData(book);
+        setDiaries(diaries);
+      }
+    };
+    fetchBookData();
+  }, [bookId]);
+
   return (
     <>
-
-    <div className={styles.container}>
-      <div className={styles.bookSection}>
-        <p className={styles.title}>{books.title}</p>
-        <img src={books.image} alt={books.image} className={styles.image} />
-        <p>{books.author} </p>
-        <p>{books.description}</p>
-      </div>
-
-      <section className={styles.recommendationSection}>
-        <h2>같은 책을 읽으신 분들의 책장이에요!</h2>
-        <div className={styles.recommendationList}>
-          {Array(5)
-            .fill(null)
-            .map((_, index) => (
-              <div key={index} className={styles.recommendationCard}>
-                <div className={styles.imgBox}>
-                  <img src="/"/>
-                  <Link to={"/mybook"}>책장 구경가기</Link>
-                </div>
-                <div className={styles.cardContent}>
-                  <h3>
-                    나만의 책제목 여기에 들어감
-                  </h3>
-                  <p>
-                    첫번째 게시글이 여기에 보여집니다. 어쩌면, 관련된 DB를
-                    쓰는게 낫지 않을까 생각해봅니다. 낫지 않을까 생각해봅니다.
-                    생각해봅니다 생각해봅니다.
-                  </p>
-                </div>
-              </div>
-            ))}
+      <div className={styles.container}>
+        <div className={styles.bookSection}>
+          <p className={styles.title}>{bookData?.title}</p>
+          <img
+            src={bookData?.image}
+            alt={bookData?.image}
+            className={styles.image}
+          />
+          <p>{bookData?.author} </p>
+          <p>{bookData?.description}</p>
         </div>
-      </section>
-    </div>
+        <DiarySection diaries={diaries} />
+      </div>
     </>
   );
 };
-
-/**
- * 유저 아바타
- * 유저 다이어리 제목
- * 유저 다이어리 0번째 포스팅팅 내용
- */

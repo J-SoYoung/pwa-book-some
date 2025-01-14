d3a16a2a3e175f7e4bb85e9debf43525c61f21ed
thdud <thgml1444@naver.com>
2025-01-14 19:08:27 +0900

    Edit : post의 id가 없는데 key로 넣고 있었음-> map의 index로 대체

--- src/pages/diaries/postItems/PostItems.tsx
+++ src/pages/diaries/postItems/PostItems.tsx
@@ -4,15 +4,17 @@ import styles from "../diaries.module.css";
 export const PostItems = ({ posts }: { posts: PostsType[] }) => {
   return (
     <>
-      {posts.map((post) => (
-        <div className={styles.postsItems} key={post.id}>
-          <div className={styles.thumbnail}></div>
-          <div className={styles.details}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
+      {posts.map((post, idx) => {
+        return (
+          <div className={styles.postsItems} key={idx}>
+            <div className={styles.thumbnail}></div>
+            <div className={styles.details}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </>
   );
 };

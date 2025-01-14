edd47be9cdba3b29ec0d6d98f9cef9603a6adc1b
thdud <thgml1444@naver.com>
2025-01-14 19:06:39 +0900

    Add : detail페이지에서 Diary내용을 컴포넌트로 분리, DiarySection으로 생성

---
+++ src/pages/detail/diarySection/DiarySection.tsx
@@ -0,0 +1,28 @@
+import styles from "./diarySection.module.css";
+import { DiaryAndPostsType } from "../Detail";
+import { Link } from "react-router-dom";
+
+export const DiarySection = ({ diaries }: { diaries: DiaryAndPostsType[] }) => {
+  return (
+    <section className={styles.recommendationSection}>
+      <h2>같은 책을 읽으신 분들의 책장이에요!</h2>
+      <div className={styles.recommendationList}>
+        {diaries.map((diary) => {
+          return (
+            <div key={diary.id} className={styles.recommendationCard}>
+              <div className={styles.imgBox}>
+                <img src={diary.bookImage} />
+                <Link to={`/diaries/${diary.id}`}>책장 구경가기</Link>
+              </div>
+              <div className={styles.cardContent}>
+                <h3>{diary.title}</h3>
+                <p className={styles.subTitle}>{diary.firstPost.title}</p>
+                <p>{diary.firstPost.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};

b1f0d5c4621126fdbc261536902829fb75c9fbe5
thdud <thgml1444@naver.com>
2025-01-14 15:04:13 +0900

    Update : 추천books database에 있는 데이터로 추가

--- src/pages/home/recommendbooks/RecommedBooks.tsx
+++ src/pages/home/recommendbooks/RecommedBooks.tsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 import styles from "./recommendations.module.css";
+import { useEffect, useState } from "react";
+import { getRandomBooks } from "@/services/apis";
+import { BookType } from "@/services/types";
 
 function Recommendations() {
+  const [books, setBooks] = useState<BookType[] | []>([]);
+
+  useEffect(() => {
+    const fetchBookData = async () => {
+      const bookData = await getRandomBooks();
+      setBooks(bookData);
+    };
+    fetchBookData();
+  }, []);
+  console.log(books);
   return (
     <section className={styles.recommendations}>
       <h2>이 책을 추천합니다</h2>
       <div className={styles.bookGrid}>
-        <Link to={"/detail"}>
-          <div className={styles.bookItem}></div>
-        </Link>
-        <Link to={"/detail"}>
-          <div className={styles.bookItem}></div>
-        </Link>
-        <Link to={"/detail"}>
-          <div className={styles.bookItem}></div>
-        </Link>
+        {books.map((book) => {
+          return (
+            <Link
+              to={`/detail/${book.id}`}
+              key={book.id}
+              className={styles.bookItem}
+            >
+              <img src={book.image} alt="book" />
+              <h3>{book.title}</h3>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );

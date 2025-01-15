17c94b32e5ed5d59a51dac1c19f3b4f43b74eb4b
thdud <thgml1444@naver.com>
2025-01-15 14:47:42 +0900

    Update : Home의 RecommendBook컴포넌트 삭제후, 기본적인 styling과 db에 데이터 요쳥하는 코드 추가, BookItem컴포넌트에 props으로 전달

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import styles from "./home.module.css";
-import Recommendations from "./recommendbooks/RecommedBooks";
+
+import { BookItems } from "@/components";
 import UserBooks from "./userBooks/UserBooks";
 
+import { getRecommendBooks } from "@/services/apis";
+import { BookType } from "@/services/types";
+
 export const Home = () => {
+  const [recommendBooks, setRecommendBooks] = useState<BookType[] | []>([]);
+
+  useEffect(() => {
+    const fetchBookData = async () => {
+      const bookData = await getRecommendBooks();
+      if (bookData) setRecommendBooks(bookData);
+    };
+    fetchBookData();
+  }, []);
+
   return (
     <main className={styles.home}>
       <h2>이 책을 추천합니다</h2>
-      <Recommendations />
+      <section className={styles.recommendations}>
+        <BookItems items={recommendBooks} types="books" />
+      </section>
 
       <h2>다른 유저들은 이런 책을 읽고 있어요!</h2>
       <UserBooks />

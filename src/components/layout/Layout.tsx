85771dbd989b390aa45039853dccdc32eeaa31d2
thdud <thgml1444@naver.com>
2025-01-15 13:26:25 +0900

    Chore : move 검색바를 레이아웃 컴포넌트로 이동, recoil의 유저 정보도 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,9 +5,14 @@ import styles from "./layout.module.css";
 
 import { useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
+import SearchBar from "../searchBar/SearchBar";
+import { useRecoilValue } from "recoil";
+import { UserType } from "@/services/types";
+import { userState } from "@/recoil/atoms";
 
 function Layout() {
   const navigate = useNavigate();
+  const user = useRecoilValue<UserType | null>(userState);
 
   useEffect(() => {
     const isStandalone = window.matchMedia(
@@ -25,7 +30,9 @@ function Layout() {
       <DesktopNavbar />
       <header className={styles.header}>
         <h1>BOOKSOME</h1>
+        <p>{user?.username}님</p>
       </header>
+      <SearchBar />
       <Outlet /> {/* 현재 라우트의 자식 컴포넌트 렌더링 */}
       <BottomNav />
     </>

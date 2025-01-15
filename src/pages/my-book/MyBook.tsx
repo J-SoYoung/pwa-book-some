bea4d78945d44e956eb0e3c47d78fec99fdc2d39
thdud <thgml1444@naver.com>
2025-01-15 14:46:39 +0900

    Refactor : Mybook의 읽고 있는 책 코드를 삭제하고 BookItem컴포넌트로 대체

--- src/pages/my-book/MyBook.tsx
+++ src/pages/my-book/MyBook.tsx
@@ -1,30 +1,34 @@
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-
+import { useNavigate } from "react-router-dom";
 import styles from "./mybook.module.css";
+
+import { BookItems } from "@/components";
+
 import { userState } from "@/recoil/atoms";
 import { getMyBookData } from "@/services/apis";
 import { DiariesType, UserType } from "@/services/types";
-import { Link, useNavigate } from "react-router-dom";
 
 export const MyBook = () => {
   const navigate = useNavigate();
   const user = useRecoilValue(userState) as UserType;
-  const [myBooksDiaries, setMyBooks] = useState<DiariesType[]>([]);
+  const [readingBookDiaries, setReadingBookDiaries] = useState<DiariesType[]>(
+    []
+  );
 
   if (!user.userId) {
     alert("로그인 후 이용 가능합니다");
     navigate("/login");
   }
+
   useEffect(() => {
     const fetchData = async () => {
       const myBookDiariesData = await getMyBookData(user.userId);
-      setMyBooks(myBookDiariesData);
+      if (myBookDiariesData) setReadingBookDiaries(myBookDiariesData);
     };
     fetchData();
   }, [user.userId]);
 
-  console.log(myBooksDiaries);
   return (
     <div className={styles.container}>
       <p>{user?.username}님 총 ()권의 책을 읽으셨네요!</p>
@@ -36,23 +40,10 @@ export const MyBook = () => {
           ))}
         </div>
       </section>
+      
       <section className={styles.section}>
         <h3>읽고 있는 책</h3>
-        <div className={styles.bookCardBox}>
-          {myBooksDiaries.map((diary) => {
-            console.log(diary);
-            return (
-              <Link
-                to={`/diaries/${diary.diaryId}`}
-                key={diary.bookId}
-                className={styles.bookCard}
-              >
-                <img src={diary.bookImage} alt={diary.bookTitle} />
-                <p>{diary.diaryTitle}</p>
-              </Link>
-            );
-          })}
-        </div>
+        <BookItems items={readingBookDiaries} types="diaries" />
       </section>
     </div>
   );

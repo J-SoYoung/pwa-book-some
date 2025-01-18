84cd4b6aa23821bc72fc1021c4670f6565984d25
thdud <thgml1444@naver.com>
2025-01-18 23:47:37 +0900

    Refactor : Layout컴포넌트에서 header내용 추출 ->> Header컴포넌트 생성

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,30 @@
+import { userState } from "@/recoil/atoms";
+import { useRecoilValue } from "recoil";
+import styles from "./header.module.css";
+import { FiSettings } from "react-icons/fi";
+
+import { UserType } from "@/services/types";
+import { Link, useNavigate } from "react-router-dom";
+
+export const Header = () => {
+  const navigate = useNavigate();
+  const user = useRecoilValue<UserType | null>(userState);
+
+  return (
+    <header className={styles.header}>
+      <h1>
+        <Link to={"/"}>BOOKSOME</Link>
+      </h1>
+      <div>
+        <span>{user?.username}님</span>
+        <FiSettings
+          size={15}
+          onClick={() => {
+            navigate(`/mypage/${user?.userId}`);
+          }}
+          className={styles.icon_setting}
+        />
+      </div>
+    </header>
+  );
+};

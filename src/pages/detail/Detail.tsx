3281045acac00d75f8b108ae688f67951d933a16
thdud <thgml1444@naver.com>
2025-01-19 13:41:14 +0900

    Update : 상세페이지 기능 추가 -> 더보기 버튼으로 description내용 줄였다가 더 보여주기, naver book으로 구매하러 가는 링크 생성

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./detail.module.css";
 import { DiarySection } from "./diarySection/DiarySection";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { BookType, DiariesType, PostsType } from "@/services/types";
 import { getBookAndDiaries } from "@/services/apis";
 
@@ -13,6 +13,7 @@ export interface DetailDataType {
 
 export const Detail = () => {
   const { bookId } = useParams<{ bookId: string }>();
+  const [isExpanded, setIsExpanded] = useState(false);
   const [bookData, setBookData] = useState<BookType | null>(null);
   const [diaries, setDiaries] = useState<DiaryAndPostsType[] | []>([]);
 
@@ -28,18 +29,41 @@ export const Detail = () => {
     fetchBookData();
   }, [bookId]);
 
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.bookSection}>
-          <p className={styles.title}>{bookData?.title}</p>
+          <div className={styles.title}>
+            <p>{bookData?.title}</p>
+            <Link
+              to={bookData?.link as string}
+              className={styles.bookLink}
+              target="_blank" // 새 탭에서 열기
+              rel="noopener noreferrer" // 보안 및 성능 향상을 위해 추가
+            >
+              구매하러가기
+            </Link>
+          </div>
           <img
             src={bookData?.image}
-            alt={bookData?.image}
+            alt={bookData?.title}
             className={styles.image}
           />
-          <p>{bookData?.author} </p>
-          <p>{bookData?.description}</p>
+          <p className={styles.author}>{bookData?.author} </p>
+          <p
+            className={
+              isExpanded ? `${styles.expanded}` : `${styles.description}`
+            }
+          >
+            {bookData?.description}
+          </p>
+          <button onClick={handleToggle}>
+            {isExpanded ? "접기" : "더보기"}
+          </button>
         </div>
         <DiarySection diaries={diaries} />
       </div>

2052236115c795eed5c317557ed515f8cd6448cb
thdud <thgml1444@naver.com>
2025-01-19 12:49:20 +0900

    Add : 내 정보 수정 페이지 생성 및 UI 렌더링, 레이아웃 분리가 필요! searchbar, main-header필요없음

--- src/pages/my-page/MyPage.tsx
+++ src/pages/my-page/MyPage.tsx
@@ -1,5 +1,41 @@
-export const MyPage=()=>{
-  return(
-    <div>Mypage</div>
-  )
-}
+import { useRecoilState } from "recoil";
+import styles from "./mypage.module.css";
+import { userState } from "@/recoil/atoms";
+
+import { MdArrowBackIosNew } from "react-icons/md";
+import { FaPen } from "react-icons/fa6";
+import { UserType } from "@/services/types";
+
+export const MyPage = () => {
+  const [user] = useRecoilState(userState);
+  const { username, email} = user as UserType;
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <MdArrowBackIosNew size={20} className={styles.backButton} />
+        <h2>내 정보 관리</h2>
+      </header>
+
+      <div className={styles.profileImageContainer}>
+        <div className={styles.profileImage}>
+          <img src={user?.avatar || ""} alt="user.username" />
+          <p className={styles.imageEditPen}>
+            <FaPen size={20} />
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.infoContainer}>
+        <div className={styles.infoRow}>
+          <span className={styles.label}>username</span>
+          <span className={styles.value}>{username}</span>
+        </div>
+        <div className={styles.infoRow}>
+          <span className={styles.label}>email</span>
+          <span className={styles.value}>{email}</span>
+        </div>
+      </div>
+      <button className={styles.editButtonLarge}>수정하기</button>
+    </div>
+  );
+};

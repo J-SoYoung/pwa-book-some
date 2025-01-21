b733dd7a877e50fe919e879668e2a4b8269c8385
thdud <thgml1444@naver.com>
2025-01-21 14:55:05 +0900

    Update : LikeButton컴포넌트 추가 - 해당 다이어리를 클릭해 좋아요를 표시한다. 좋아요 기능이 있는 LikeBUtton컴포넌트 생성 후 UI로 렌더링 함;

--- src/pages/home/userBooks/UserBooks.tsx
+++ src/pages/home/userBooks/UserBooks.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./userBooks.module.css";
+
 import { getAllBookDiaries } from "@/services/apis";
 import { DiariesWithPostsType } from "@/services/types";
-import { useNavigate } from "react-router-dom";
+import { LikeButton } from "@/components/likeButton/LikeButton";
 
 function UserBooks() {
   const navigate = useNavigate();
@@ -21,21 +23,20 @@ function UserBooks() {
       <div>
         {bookDiaries.map((diary) => {
           return (
-            <div
-              className={styles.bookCard}
-              key={diary.diaryId}
-              onClick={() => {
-                navigate(`/diaries/${diary.diaryId}`);
-              }}
-            >
+            <div className={styles.bookCard} key={diary.diaryId}>
               <img src={diary.bookImage} className={styles.bookImage} />
+              <div className={styles.likeBox}>
+                <LikeButton diaryId={diary.diaryId} />
+              </div>
 
-              <div className={styles.bookInfo}>
+              <div
+                className={styles.bookInfo}
+                onClick={() => {
+                  navigate(`/diaries/${diary.diaryId}`);
+                }}
+              >
                 <h3>{diary.diaryTitle}</h3>
                 <p>{diary.bookTitle}</p>
-                <div className={styles.likeBox}>
-                  <p>❤️📒</p>
-                </div>
                 <p>{diary.posts[0].content}</p>
               </div>
             </div>

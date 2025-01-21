d5866e006f822aa22d59d8487831e2c76807100b
thdud <thgml1444@naver.com>
2025-01-21 15:00:50 +0900

    Add : 좋아요 기능 구현=> updateDiaryLikeState : firebase에 likes db테이블에 boolean데이터 추가,likeSubscribe : 좋아요 상태를 구독하기 위해 함수추가 - updateDiaryLikeState: 넘어온 likeState의 boolean값을 확인하여 remove, set메서드를 실행시킨다 - likeSubscribe: onValue 구독 메서드를 사용하여 해당 다이어리와 user의 좋아요 상태를 구독해 state에 업데이트 시킨다

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,4 +1,4 @@
-import { get, ref, set, update } from "firebase/database";
+import { get, onValue, ref, remove, set, update } from "firebase/database";
 import { database } from "./firebase";
 import {
   BookType,
@@ -221,4 +221,39 @@ export const getSearchResults = async (query: string) => {
     console.error("검색 결과 가져오기 에러", error);
     return [];
   }
+};
+
+// 다이어리 좋아요
+export const updateDiaryLikeState = async (
+  likeState: boolean,
+  diaryId: string,
+  userId: string
+) => {
+  try {
+    const likeRef = ref(database, `likes/${diaryId}/${userId}`);
+    if (likeState) {
+      await remove(likeRef);
+      return false;
+    } else {
+      await set(likeRef, true);
+      return true;
+    }
+  } catch (error) {
+    console.error("좋아요 기능 에러", error);
+    return false;
+  }
+};
+
+export const likeSubscribe = (
+  diaryId: string,
+  userId: string,
+  setLikes: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  const likeRef = ref(database, `likes/${diaryId}/${userId}`);
+
+  // 좋아요 상태 확인 ( 실시간 상태 확인 )
+  const unsubscribe = onValue(likeRef, (snapshot) => {
+    setLikes(snapshot.exists());
+  });
+  return unsubscribe;
 };

7a9e73f4bb1fcf5c8f6d758c69d6b90d15d6bb66
thdud <thgml1444@naver.com>
2025-01-29 16:45:09 +0900

    Update : 개별 포스트의 수정 기능 추가, 본인이 작성한 post에 수정 버튼 생성 - map으로 post의 배열 데이터를 UI로 반환하는 기능 -> 각 포스트의 수정 기능을 포함한 데이터 반환으로 업데이트 - InputEditField 컴포넌트를 사용해 해당 데이터의 수정 form 생성 - isEditPost의 값에 따라 input필드 or post데이터 를 반환 - 수정 버튼을 클릭해 해당 데이터를 수정하고 저장하는 기능 구현됨

--- src/pages/diaries/postItems/PostItems.tsx
+++ src/pages/diaries/postItems/PostItems.tsx
@@ -1,20 +1,105 @@
-import { PostsType } from "@/services/types";
+import { useState } from "react";
 import styles from "../diaries.module.css";
+import { PostsType } from "@/services/types";
+import { validateEditPost } from "@/services/utils";
+import { updatePosts } from "@/services/apis";
+import { InputEditField } from "@/components";
 
-export const PostItems = ({ posts }: { posts: PostsType[] }) => {
+interface PostsPropsType {
+  post: PostsType;
+  isAuthor: boolean;
+  setPosts: React.Dispatch<React.SetStateAction<PostsType[] | []>>;
+}
+export interface newPostType {
+  title: string;
+  content: string;
+  updatedAt: string;
+}
+
+export const PostItems = ({ post, setPosts, isAuthor }: PostsPropsType) => {
+  const [isEditPost, setIsEditPost] = useState(false);
+  const [editPost, setEditPost] = useState({
+    title: post.title,
+    content: post.content,
+    updatedAt: new Date().toISOString()
+  });
+
+  const handleChangePosts = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setEditPost({ ...editPost, [e.target.name]: e.target.value });
+  };
+  const handlePostEdit = () => {
+    setIsEditPost(!isEditPost);
+  };
+
+  const handlePostSave = async () => {
+    if (editPost.title === "") editPost.title = post.title;
+    if (editPost.content === "") editPost.content = post.content;
+
+    const validate = validateEditPost(editPost);
+    if (!validate.valid) return alert(validate.message);
+
+    try {
+      const response = await updatePosts({
+        editPost: editPost as newPostType,
+        postId: post.postId as string
+      });
+      console.log("response--", response);
+
+      if (response) {
+        setPosts((prevPost) =>
+          prevPost.map((p) =>
+            p.postId === post.postId
+              ? { ...p, title: response.title, content: response.content }
+              : p
+          )
+        );
+
+        setIsEditPost(false);
+      }
+    } catch (error) {
+      console.error(error, "포스트 수정 에러");
+    }
+  };
+
   return (
-    <>
-      {posts.map((post, idx) => {
-        return (
-          <div className={styles.postsItems} key={idx}>
-            <div className={styles.thumbnail}></div>
-            <div className={styles.details}>
-              <h3>{post.title}</h3>
-              <p>{post.content}</p>
-            </div>
-          </div>
-        )
-      })}
-    </>
+    <div className={styles.postsItems}>
+      <div className={styles.details}>
+        {isEditPost ? (
+          <>
+            <InputEditField
+              defaultValue={post.title}
+              name={"title"}
+              onChange={handleChangePosts}
+              inputType={"input"}
+            />
+            <InputEditField
+              defaultValue={post.content}
+              name={"content"}
+              onChange={handleChangePosts}
+              inputType={"textarea"}
+            />
+          </>
+        ) : (
+          <>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </>
+        )}
+      </div>
+      {isAuthor && (
+        <div>
+          {isEditPost ? (
+            <>
+              <button onClick={handlePostSave}>저장</button>
+              <button onClick={handlePostEdit}>취소</button>
+            </>
+          ) : (
+            <button onClick={handlePostEdit}>수정</button>
+          )}
+        </div>
+      )}
+    </div>
   );
 };

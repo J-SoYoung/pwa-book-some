b169fcaab42fef1bdd2ce581ad31f3adee825b0d
thdud <thgml1444@naver.com>
2025-01-29 16:49:40 +0900

    Add : 수정데이터 유효성검사 함수 추가

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,6 @@
 import { get, ref } from "firebase/database";
 import { database } from "./firebase";
+import { newPostType } from "@/pages/diaries/postItems/PostItems";
 
 // 배열을 랜덤하게 섞어주는 함수
 export const shuffleArray = <T>(array: T[]): T[] => {
@@ -78,4 +79,17 @@ export const validateValue = (data: string) => {
   } else {
     return { valid: true, message: "" };
   }
+};
+
+export const validateEditPost = (editPost: newPostType) => {
+  const { title, content } = editPost;
+
+  if (title === "" || title.length < 5) {
+    return { valid: false, message: "포스트 제목은 5자 이상이어야 합니다" };
+  }
+  if (content === "" || content.length < 20) {
+    return { valid: false, message: "포스트 내용은 20자 이상이어야 합니다" };
+  }
+
+  return { valid: true, message: "", editPost };
 };

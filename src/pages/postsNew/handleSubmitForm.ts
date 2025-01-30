61845375acf578ba893c8c0a112fd0fa23ceb305
thdud <thgml1444@naver.com>
2025-01-31 00:52:24 +0900

    Updage : 다이어리 커버 이미지 추가할 수 있도록 submit함수 업데이트

--- src/pages/postsNew/handleSubmitForm.ts
+++ src/pages/postsNew/handleSubmitForm.ts
@@ -4,14 +4,20 @@ import { validatePostsNewForm } from "@/services/utils";
 import { NavigateFunction } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
+export interface diaryDataType {
+  diaryTitle: string;
+  todayTitle: string;
+  content: string;
+}
+
 export const handleSubmitForm = async (
   e: React.FormEvent<HTMLFormElement>,
   diaryData: {
     diaryTitle: string;
     todayTitle: string;
     content: string;
-    diaryImage: string;
   },
+  image: string,
   selectedBook: SelectedBookType,
   user: UserType,
   navigate: NavigateFunction
@@ -40,7 +46,7 @@ export const handleSubmitForm = async (
         diaries: {
           diaryId: diaryId,
           diaryTitle: diaryData.diaryTitle,
-          diaryImage: "/",
+          diaryImage: image,
           createdAt: new Date().toISOString()
         },
         posts: {
@@ -48,16 +54,18 @@ export const handleSubmitForm = async (
           createdAt: new Date().toISOString(),
           diaryId: diaryId,
           postId: postId,
-          title: diaryData.todayTitle,
+          title: diaryData.todayTitle
         },
         user: user as UserType
       };
       const result = await createNewDiaryPost(newDiaryData);
-      if (result) navigate("/home");
+      if (result) {
+        alert(result);
+        navigate("/home");
+      }
     }
   } catch (error) {
     console.error(error);
     alert("포스팅 작성중 에러가 발생했습니다. 다시 시도해주세요요");
   }
 };
-

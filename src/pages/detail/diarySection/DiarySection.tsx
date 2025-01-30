6a7d0f095dcc9182e084b478b9bf10c5271b6d4e
thdud <thgml1444@naver.com>
2025-01-30 22:36:15 +0900

    Chore : 다이어리를 쓴 유저 데이터를 추가하여 UI에 유저정보를 보여줌

--- src/pages/detail/diarySection/DiarySection.tsx
+++ src/pages/detail/diarySection/DiarySection.tsx
@@ -1,8 +1,14 @@
-import styles from "./diarySection.module.css";
-import { DiaryWidthPostsType } from "../Detail";
 import { Link } from "react-router-dom";
 
-export const DiarySection = ({ diaries }: { diaries: DiaryWidthPostsType[] }) => {
+import styles from "./diarySection.module.css";
+import { DiaryWidthPostsType } from "@/services/types";
+
+
+export const DiarySection = ({
+  diaries
+}: {
+  diaries: DiaryWidthPostsType[];
+}) => {
   return (
     <section className={styles.recommendationSection}>
       <h2>같은 책을 읽으신 분들의 책장이에요!</h2>
@@ -11,13 +17,16 @@ export const DiarySection = ({ diaries }: { diaries: DiaryWidthPostsType[] }) =>
           return (
             <div key={diary.diaryId} className={styles.recommendationCard}>
               <div className={styles.imgBox}>
-                <img src={diary.diaryImage} />
-                <Link to={`/diaries/${diary.diaryId}`}>책장 구경가기</Link>
+                <img src={diary.userAvatar} />
+                <Link to={`/diaries/${diary.diaryId}`}>
+                  {diary.username}님<br />
+                  책장 구경가기
+                </Link>
               </div>
               <div className={styles.cardContent}>
                 <h3>{diary.diaryTitle}</h3>
                 <p className={styles.subTitle}>{diary.postTitle}</p>
-                <p>{diary.postContent}</p>
+                <p className={styles.content}>{diary.postContent}</p>
               </div>
             </div>
           );

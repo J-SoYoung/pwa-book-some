0e56866fcd7c65608e7067dfad685039216288ab
thdud <thgml1444@naver.com>
2025-01-30 22:34:18 +0900

    Add : BookItems의 관심사 분리로 배열 데이터를 props으로 받고 Items에 들어갈 데이터를 처리하는 역할의 컴포넌트다. - 태그 명이 <section>으로 되어 있어 하나의 section을 관리할 수 있도록 했다.

---
+++ src/pages/my-book/ItemLists.tsx
@@ -0,0 +1,37 @@
+import { DiariesType } from "@/services/types";
+import { Items } from "@/components/items/Items";
+import styles from "./mybook.module.css";
+
+interface ItemListsProps {
+  sectionTitle: string;
+  diaries: DiariesType[];
+  noDataText: string;
+}
+
+export const ItemLists = ({
+  sectionTitle,
+  diaries,
+  noDataText
+}: ItemListsProps) => {
+  return (
+    <section className={styles.section}>
+      <h3>{sectionTitle}</h3>
+      <div className={styles.itemListsBox}>
+        {diaries.length > 0 ? (
+          <>
+            {diaries.map((diary) => {
+              const data = {
+                url: `/diaries/${diary.diaryId}`,
+                imageUrl: diary.bookImage,
+                title: diary.diaryTitle
+              };
+              return <Items data={data} key={diary.diaryId} />;
+            })}
+          </>
+        ) : (
+          <p>{noDataText}</p>
+        )}
+      </div>
+    </section>
+  );
+};

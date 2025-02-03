50576dc4a8387048fe931f49258981ed10fb8591
thdud <thgml1444@naver.com>
2025-02-03 12:12:44 +0900

    Refactor : Diary페이지에서 Post배열을 렌더링하는 부분을 컴포넌트로 분리, post데이터 API 호출을 해당 컴포넌트로 넘김과 동시에 사용하지 않는 코드 삭제

--- src/pages/diaries/Diaries.tsx
+++ src/pages/diaries/Diaries.tsx
@@ -3,39 +3,31 @@ import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import styles from "./diaries.module.css";
 
-import { PostItems } from "./components/PostItems";
 import { DiaryItem } from "./components/DiaryItem";
+import { PostLists } from "./components/PostLists";
 
-import { getDiaryPosts } from "@/services/apis";
-import { DiariesType, PostsType } from "@/services/types";
+import { getDiaryData } from "@/services/apis";
+import { DiariesType } from "@/services/types";
 import { userState } from "@/recoil/atoms";
-
-type FetchResultType = {
-  postsData: PostsType[];
-  diaryData: DiariesType | null;
-};
 
 export const Diaries = () => {
   const { diaryId } = useParams<{ diaryId: string }>();
-
   const users = useRecoilValue(userState);
-  const [posts, setPosts] = useState<PostsType[]>([]);
-  const [diary, setDiary] = useState<DiariesType | null>(null);
   const [isAuthor, setIsAuthor] = useState(false);
+  const [diary, setDiary] = useState<DiariesType | null>(null);
 
   useEffect(() => {
     const fetchPosts = async (diaryId: string) => {
       try {
-        const response = (await getDiaryPosts(diaryId)) as FetchResultType;
-        setPosts(response.postsData);
-        setDiary(response.diaryData);
+        const diaryData = await getDiaryData(diaryId);
+        setDiary(diaryData);
       } catch (error) {
         console.error("다이어리 가져오기 에러", error);
       }
     };
+    
     if (diary?.userId === users?.userId) setIsAuthor(true);
 
-    console.log(diary);
     fetchPosts(diaryId as string);
   }, [diaryId, users?.userId, diary?.userId]);
 
@@ -43,17 +35,7 @@ export const Diaries = () => {
     <main className={styles.diariesContainer}>
       <h2>Diaries</h2>
       <DiaryItem diary={diary} setDiary={setDiary} isAuthor={isAuthor} />
-
-      {posts.map((post) => {
-        return (
-          <PostItems
-            key={post.postId}
-            post={post}
-            setPosts={setPosts}
-            isAuthor={isAuthor}
-          />
-        );
-      })}
+      <PostLists diaryId={diaryId as string} isAuthor={isAuthor} />
     </main>
   );
 };

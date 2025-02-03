deb9beba7a9c055aa04124220b1b09532992cb86
thdud <thgml1444@naver.com>
2025-02-03 12:13:49 +0900

    Add : PostLists컴포넌트 생성, 현재 페이지에서 post정보를 받아와 Items컴포넌트에 map을 사용해 각 데이터를 props으로 전달

---
+++ src/pages/diaries/components/PostLists.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { PostsType } from "@/services/types";
+import { getPostsData } from "@/services/apis";
+import { PostItems } from "./PostItems";
+
+interface PostListsProps {
+  diaryId: string;
+  isAuthor: boolean;
+}
+export const PostLists = ({ diaryId, isAuthor }: PostListsProps) => {
+  const [posts, setPosts] = useState<PostsType[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async (diaryId: string) => {
+      try {
+        const postData = await getPostsData(diaryId as string);
+        setPosts(postData);
+      } catch (error) {
+        console.error("다이어리 가져오기 에러", error);
+      }
+    };
+    fetchPosts(diaryId as string);
+  }, [diaryId]);
+
+  return (
+    <>
+      {posts.map((post) => {
+        return (
+          <PostItems
+            key={post.postId}
+            post={post}
+            setPosts={setPosts}
+            isAuthor={isAuthor}
+          />
+        );
+      })}
+    </>
+  );
+};

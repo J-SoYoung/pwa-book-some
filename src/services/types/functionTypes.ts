8b00bc26273e85f2847e77e320624ede34e392e9
thdud <thgml1444@naver.com>
2025-02-06 00:34:39 +0900

    Refactor : 함수표현식에 타입을 지정함. ts파일에 지정해두고 api파일에서 import해서 사용할 수 있도록 지정함 -> 함수의 타입이 깔끔하게 정리됨

--- src/services/types/functionTypes.ts
+++ src/services/types/functionTypes.ts
@@ -1,5 +1,27 @@
-import { BookType, DiariesType, PostsType } from "./dataTypes";
+import {
+  AllDiariesDataType,
+  BookType,
+  DiariesType,
+  DiaryWithPostsType,
+  DiaryWithUserType,
+  PostsType,
+  UserType
+} from "./dataTypes";
 
-export type GetBookDataType = (bookId: string) => Promise<BookType | null>;
+export type GetBookDataType = (bookIsbn: string) => Promise<BookType | null>;
+
 export type GetDiaryDataType = (diaryId: string) => Promise<DiariesType | null>;
+
 export type GetPostsDataType = (bookId: string) => Promise<PostsType[] | []>;
+
+export type GetUserDataType = (userId: string) => Promise<UserType | null>;
+
+export type getBookWithDiaryPostType = (
+  bookId: string
+) => Promise<DiaryWithPostsType[] | []>;
+
+export type GetDiaryWithUserDataType = (
+  diaryId: string
+) => Promise<DiaryWithUserType | null>;
+
+export type GetAllDiariesType = () => Promise<AllDiariesDataType[] | []>;

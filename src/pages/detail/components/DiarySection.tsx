887a06dd29ab83b7d048a77c860eb28e6640471a
thdud <thgml1444@naver.com>
2025-02-06 14:20:47 +0900

    Refactor : 객체 형식으로 데이터를 받아옴에 따라 UI에서 사용하는 데이터도 업데이트

--- src/pages/detail/components/DiarySection.tsx
+++ src/pages/detail/components/DiarySection.tsx
@@ -18,23 +18,23 @@ export const DiarySection = () => {
     };
     fetchBookData();
   }, [bookIsbn]);
-
   return (
     <div className={styles.diaryList}>
-      {diaries.map((diary) => {
+      {diaries.map((diaryData) => {
+        const { diary, post, user } = diaryData;
         return (
           <div key={diary.diaryId} className={styles.diary}>
             <div className={styles.imgBox}>
-              <img src={diary.userAvatar} />
+              <img src={user.avatar} />
               <Link to={`/diaries/${diary.diaryId}`}>
-                {diary.username}님<br />
+                {user.username}님<br />
                 책장 구경가기
               </Link>
             </div>
             <div className={styles.contentBox}>
               <h3>{diary.diaryTitle}</h3>
-              <p className={styles.subTitle}>{diary.postTitle}</p>
-              <p className={styles.content}>{diary.postContent}</p>
+              <p className={styles.subTitle}>{post.title}</p>
+              <p className={styles.content}>{post.content}</p>
             </div>
           </div>
         );

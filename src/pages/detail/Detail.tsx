054bd3ce06aac6b80cdf47bb7e7f604cd911aa50
thdud <thgml1444@naver.com>
2025-02-07 10:49:01 +0900

    Refactor : Detail의 컴포넌트에서 공통으로 사용하는 bookIsbn을 값을 props으로 넘겨줌

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,13 +1,16 @@
 import { DiarySection } from "./components/DiarySection";
 import { BookSection } from "./components/BookSection";
+import { useParams } from "react-router-dom";
 
 export const Detail = () => {
+  const { bookIsbn } = useParams<{ bookIsbn: string }>();
+
   return (
     <>
-      <BookSection />
+      <BookSection bookIsbn={bookIsbn as string} />
       <section>
         <h2>같은 책을 읽으신 분들의 책장이에요!</h2>
-        <DiarySection />
+        <DiarySection bookIsbn={bookIsbn as string} />
       </section>
     </>
   );

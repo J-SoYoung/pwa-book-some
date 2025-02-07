396c1567b9c4cdd6bacaf8db9f53fb6ed5eef7cb
thdud <thgml1444@naver.com>
2025-02-07 10:14:41 +0900

    Refactor : data상태관리를 useEffect + state => react-query로 변경, useQeury로 데이터 상태관리 시작

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,22 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import styles from "./home.module.css";
-
 import UserBooks from "./components/UserBooks";
 
-import { getRecommendBooks } from "@/services/apis";
-import { BookType } from "@/services/types/dataTypes";
 import { Items } from "@/components/items/Items";
+import { getRecommendBooks } from "@/services/apis";
 
 export const Home = () => {
-  const [recommendBooks, setRecommendBooks] = useState<BookType[] | []>([]);
-
-  useEffect(() => {
-    const fetchBookData = async () => {
-      const bookData = await getRecommendBooks();
-      if (bookData) setRecommendBooks(bookData);
-    };
-    fetchBookData();
-  }, []);
-
+  const { data:recommendBooks} = useQuery({
+    queryKey: ["recommendedBooks"],
+    queryFn: getRecommendBooks
+  });
+  
   return (
     <main className={styles.home}>
       <section className={styles.section}>
         <h3>이 책을 추천합니다</h3>
         <div className={styles.itemListBox}>
-          {recommendBooks.map((book) => {
+          {recommendBooks?.map((book) => {
             const data = {
               url: `/detail/${book.isbn}`,
               imageUrl: book.image,

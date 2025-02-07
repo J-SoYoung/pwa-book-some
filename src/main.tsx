f1a57277bd570c64d530f98813956f88564b69e8
thdud <thgml1444@naver.com>
2025-02-07 09:50:52 +0900

    Add : main.tsx에 react-query 객체 생성 및 queryClient주입

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./routers/index.tsx";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
 );

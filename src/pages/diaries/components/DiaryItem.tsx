ad3cb6f7c4e9421fb0151b3e13a370c95a4c48f2
thdud <thgml1444@naver.com>
2025-02-10 11:05:36 +0900

    Refactor : react-query의 useMutation을 사용해 다이어리의 수정을 업데이트 할 수 있게 하였다. 비동기 처리의 함수를 mutation내부에서 처리하여 해당 상태를 관리할 수 있게 하고, queryClient를 사용해 캐시 데이터를 지우고 자동 업데이트 되도록 설정하였다.

--- src/pages/diaries/components/DiaryItem.tsx
+++ src/pages/diaries/components/DiaryItem.tsx
@@ -5,70 +5,67 @@ import { DiaryWithUserType } from "@/services/types/dataTypes";
 import { updateDiary } from "@/services/apis";
 import { InputEditField } from "@/components";
 import { validateValue } from "@/services/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface DiaryPropsType {
   diary: DiaryWithUserType | null;
   isAuthor: boolean;
-  setDiary: React.Dispatch<React.SetStateAction<DiaryWithUserType | null>>;
 }
 
-export const DiaryItem = ({ diary, isAuthor, setDiary }: DiaryPropsType) => {
-
+export const DiaryItem = ({ diary, isAuthor }: DiaryPropsType) => {
   const [isEditDiary, setIsEditDiary] = useState(false);
-  const [editDiaryTItle, setEditDiaryTitle] = useState("");
+  const [editDiaryTitle, setEditDiaryTitle] = useState("");
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: updateDiary,
+    onSuccess: () => {
+      if (diary?.diaryId) {
+        queryClient.invalidateQueries({ queryKey: ["diary", diary.diaryId] });
+      }
+      setIsEditDiary(false);
+    },
+    onError: (error) => {
+      console.error("다이어리 수정 오류:", error);
+      alert("다이어리 수정 중 오류가 발생했습니다.");
+    }
+  });
+
   const handleDiaryEdit = () => {
     setIsEditDiary(!isEditDiary);
   };
 
   const handleDiarySave = async () => {
-    const validate = validateValue(editDiaryTItle);
+    const validate = validateValue(editDiaryTitle);
     if (!validate.valid) return alert(validate.message);
 
-    try {
-      const response = await updateDiary({
-        diaryId: diary?.diaryId as string,
-        newTitle: editDiaryTItle
-      });
-
-      setDiary((prevDiary) => ({
-        ...(prevDiary as DiaryWithUserType),
-        diaryTitle: response.diaryTitle as string
-      }));
-
-      setIsEditDiary(false); // 수정 모드 종료
-    } catch (error) {
-      console.error(error, "다이어리 제목 수정 에러");
-    }
+    mutation.mutate({
+      diaryId: diary?.diaryId as string,
+      newTitle: editDiaryTitle
+    });
   };
 
   return (
     <div className={styles.diaryContainer}>
-      <div>
-        {isEditDiary ? (
+      {isEditDiary ? (
+        <div className={styles.editInput}>
           <InputEditField
             defaultValue={diary?.diaryTitle as string}
             name={"title"}
             onChange={(e) => setEditDiaryTitle(e.target.value)}
             inputType={"input"}
           />
-        ) : (
-          <div className={styles.titleBox}>
-            <h3>{diary?.diaryTitle}</h3>
-            {isAuthor && (
-              <div className={styles.buttonBox}>
-                {isEditDiary ? (
-                  <>
-                    <button onClick={handleDiarySave}>저장</button>
-                    <button onClick={handleDiaryEdit}>취소</button>
-                  </>
-                ) : (
-                  <button onClick={handleDiaryEdit}>수정</button>
-                )}
-              </div>
-            )}
+          <div>
+            <button onClick={handleDiarySave}>저장</button>
+            <button onClick={handleDiaryEdit}>취소</button>
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className={styles.diaryTitleView}>
+          <h3>{diary?.diaryTitle}</h3>
+          {isAuthor && <button onClick={handleDiaryEdit}>수정</button>}
+        </div>
+      )}
 
       <img src={diary?.diaryImage} className={styles.bgDiaryImage} />
 

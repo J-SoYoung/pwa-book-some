026fcac235a359d5177f01c619bb98ffdc87e9d3
thdud <thgml1444@naver.com>
2025-02-10 11:03:07 +0900

    Refactor : react-query를 사용해 데이터를 가져옴 -> effect와 State를 사용하지 않고 query로 데이터 상태관리 할 수 있게 함

--- src/pages/diaries/Diaries.tsx
+++ src/pages/diaries/Diaries.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import styles from "./diaries.module.css";
@@ -8,33 +8,29 @@ import { PostLists } from "./components/PostLists";
 
 import { userState } from "@/recoil/atoms";
 import { getDiaryWithUserData } from "@/services/apis";
-import { DiaryWithUserType } from "@/services/types/dataTypes";
 
 export const Diaries = () => {
   const { diaryId } = useParams<{ diaryId: string }>();
   const user = useRecoilValue(userState);
-  const [isAuthor, setIsAuthor] = useState(false);
-  const [diary, setDiary] = useState<DiaryWithUserType | null>(null);
 
-  useEffect(() => {
-    const fetchPosts = async (diaryId: string) => {
-      try {
-        const diaryWithUserData = await getDiaryWithUserData(diaryId);
-        setDiary(diaryWithUserData);
-      } catch (error) {
-        console.error("다이어리 가져오기 에러", error);
-      }
-    };
+  const {
+    data: diary,
+    isLoading,
+    error
+  } = useQuery({
+    queryKey: ["diary", diaryId],
+    queryFn: () => getDiaryWithUserData(diaryId as string),
+    enabled: !!diaryId // diaryId가 존재할 때만 실행
+  });
+  const isAuthor = diary?.user.userId === user?.userId;
 
-    if (diary?.user.userId === user?.userId) setIsAuthor(true);
-
-    fetchPosts(diaryId as string);
-  }, [diaryId, user?.userId, diary?.user.userId]);
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>에러 발생</div>;
 
   return (
     <main className={styles.diariesContainer}>
       <h2>Diaries</h2>
-      <DiaryItem diary={diary} setDiary={setDiary} isAuthor={isAuthor} />
+      {diary && <DiaryItem diary={diary} isAuthor={isAuthor} />}
       <PostLists diaryId={diaryId as string} isAuthor={isAuthor} />
     </main>
   );

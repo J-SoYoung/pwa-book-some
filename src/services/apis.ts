5b83e1b53019ddd5db9e87b6b59418f162776a8b
thdud <thgml1444@naver.com>
2025-02-10 11:09:31 +0900

    Chore : db에서 가져온 데이터가 없을 경우 null을 반환하도록 if문 작성

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -301,26 +301,28 @@ export const getDiaryWithUserData: GetDiaryWithUserDataType = async (
 ) => {
   try {
     const diaryData = await getDiaryData(diaryId);
-    if (diaryData) {
-      const userData = await getUserData(diaryData.userId);
-      const diaryWithUserData = {
-        book: {
-          isbn: diaryData.book.isbn ?? "",
-          image: diaryData.book.image ?? "",
-          title: diaryData.book.title ?? ""
-        },
-        diaryId: diaryData.diaryId,
-        diaryImage: diaryData.diaryImage ?? "",
-        diaryTitle: diaryData.diaryTitle ?? "",
-        postId: diaryData.postId ?? {},
-        user: {
-          userId: userData?.userId ?? "",
-          avatar: userData?.avatar ?? "",
-          username: userData?.username ?? ""
-        }
-      };
-      return diaryWithUserData;
-    } else return null;
+    if (!diaryData) return null;
+
+    const userData = await getUserData(diaryData.userId);
+    if (!userData) return null;
+    
+    const diaryWithUserData = {
+      book: {
+        isbn: diaryData.book.isbn ?? "",
+        image: diaryData.book.image ?? "",
+        title: diaryData.book.title ?? ""
+      },
+      diaryId: diaryData.diaryId,
+      diaryImage: diaryData.diaryImage ?? "",
+      diaryTitle: diaryData.diaryTitle ?? "",
+      postId: diaryData.postId ?? {},
+      user: {
+        userId: userData?.userId ?? "",
+        avatar: userData?.avatar ?? "",
+        username: userData?.username ?? ""
+      }
+    };
+    return diaryWithUserData;
   } catch (error) {
     console.error("getDiaryWidthUserData 에러", error);
     return null;

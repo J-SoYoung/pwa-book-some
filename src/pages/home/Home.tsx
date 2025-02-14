6553b042bc1a6d562b47cd330abca484ea485296
thdud <thgml1444@naver.com>
2025-02-14 14:43:12 +0900

    Update [ Home, UserBooks ] : useQuery의 isLoading의 값이 true인 경우 스켈레톤 UI 컴포넌트를 보여주는 삼항연산자 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import styles from "./home.module.css";
-import UserBooks from "./components/UserBooks";
 
 import { Items } from "@/components/items/Items";
 import { getRecommendBooks } from "@/services/apis";
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/recoil/atoms";
-import { UserType } from "@/services/types/dataTypes";
+import { BookType, UserType } from "@/services/types/dataTypes";
+import { ItemSkeleton } from "@/components/items/ItemSkeleton";
+import UserBooks from "./components/UserBooks";
 
 export const Home = () => {
   const user = useRecoilValue(userState) as UserType;
@@ -18,24 +19,42 @@ export const Home = () => {
     }
   });
 
-  const { data: recommendBooks } = useQuery({
+  const {
+    data: recommendBooks,
+    isLoading,
+  } = useQuery({
     queryKey: ["recommendedBooks"],
     queryFn: getRecommendBooks
   });
+
+  // const {
+  //   data: recommendBooks,
+  //   isError,
+  //   errorMessage,
+  //   SkeletonUI
+  // } = useCustomQuery(
+  //   ["recommendedBooks"],
+  //   () => getRecommendBooks(),
+  //   <ItemSkeleton />
+  // );
 
   return (
     <main className={styles.home}>
       <section className={styles.section}>
         <h3>이 책을 추천합니다</h3>
         <div className={styles.itemListBox}>
-          {recommendBooks?.map((book) => {
-            const data = {
-              url: `/detail/${book.isbn}`,
-              imageUrl: book.image,
-              title: book.title
-            };
-            return <Items data={data} key={book.isbn} />;
-          })}
+          {isLoading ? (
+            <ItemSkeleton />
+          ) : (
+            recommendBooks?.map((book: BookType) => {
+              const data = {
+                url: `/detail/${book.isbn}`,
+                imageUrl: book.image,
+                title: book.title
+              };
+              return <Items data={data} key={book.isbn} />;
+            })
+          )}
         </div>
       </section>
 
--- src/pages/home/components/UserBooks.tsx
+++ src/pages/home/components/UserBooks.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+
 import styles from "./userBooks.module.css";
+import { UserBookSkeleton } from "./UserBookSkeleton";
 
 import { getAllkDiaries } from "@/services/apis";
 import { LikeButton } from "@/components/likeButton/LikeButton";
@@ -8,46 +10,50 @@ import { LikeButton } from "@/components/likeButton/LikeButton";
 function UserBooks() {
   const navigate = useNavigate();
 
-  const { data: allDiaries } = useQuery({
+  const { data: allDiaries, isLoading } = useQuery({
     queryKey: ["allDiaries"],
     queryFn: getAllkDiaries
   });
 
   return (
     <section className={styles.userBooks}>
-      {allDiaries?.map((diaryData) => {
-        const { book, diary, post, user } = diaryData;
-        return (
-          <div className={styles.bookCard} key={diary.diaryId}>
-            <div className={styles.imageBox}>
-              <img
-                className={styles.bookImage}
-                src={book.image}
-                alt={book.title}
-              />
-              <img
-                className={styles.userAvatar}
-                src={user.avatar}
-                alt={`${user.username}의 다이어리`}
-              />
-            </div>
-            <div className={styles.likeBox}>
-              <LikeButton diaryId={diary.diaryId} />
-            </div>
+      {isLoading ? (
+        <UserBookSkeleton />
+      ) : (
+        allDiaries?.map((diaryData) => {
+          const { book, diary, post, user } = diaryData;
+          return (
+            <div className={styles.bookCard} key={diary.diaryId}>
+              <div className={styles.imageBox}>
+                <img
+                  className={styles.bookImage}
+                  src={book.image}
+                  alt={book.title}
+                />
+                <img
+                  className={styles.userAvatar}
+                  src={user.avatar}
+                  alt={`${user.username}의 다이어리`}
+                />
+              </div>
+              <div className={styles.likeBox}>
+                <LikeButton diaryId={diary.diaryId} />
+              </div>
 
-            <div
-              className={styles.bookInfo}
-              onClick={() => {
-                navigate(`/diaries/${diary.diaryId}`);
-              }}
-            >
-              <h3>{diary.diaryTitle}</h3>
-              <p>{post.title}</p>
-              <p>{post.content}</p>
+              <div
+                className={styles.bookInfo}
+                onClick={() => {
+                  navigate(`/diaries/${diary.diaryId}`);
+                }}
+              >
+                <h3>{diary.diaryTitle}</h3>
+                <p>{post.title}</p>
+                <p>{post.content}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </section>
   );
 }

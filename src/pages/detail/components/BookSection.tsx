41c5cf0a20aa878ac347d61d59445612d2761d74
thdud <thgml1444@naver.com>
2025-02-17 10:09:29 +0900

    Update [Detail Components] : Detail페이지의 Book/Diary컴포넌트의 데이터를 useCustomQueryHook을 사용해 로드함, 동시에 Skeleton, ERror를 표현할 수 있도록 UI에 업데이트 시킴

--- src/pages/detail/components/BookSection.tsx
+++ src/pages/detail/components/BookSection.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import styles from "./bookSection.module.css";
 
 import { getOneBookData } from "@/services/apis";
+import { useCustomQueryhook } from "@/pages/home/useCustomQueryHooks";
+
+const BookSkeleton = () => {
+  return (
+    <div className={styles.bookSkeleton}>
+      <div className={styles.title}></div>
+      <div className={styles.image} />
+      <p className={styles.author}></p>
+      <p className={styles.description}></p>
+    </div>
+  );
+};
 
 export const BookSection = ({ bookIsbn }: { bookIsbn: string }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -11,35 +22,50 @@ export const BookSection = ({ bookIsbn }: { bookIsbn: string }) => {
     setIsExpanded(!isExpanded);
   };
 
-  const { data: book } = useQuery({
+  const {
+    data: book,
+    SkeletonUI,
+    errorMessage
+  } = useCustomQueryhook({
     queryKey: ["book", bookIsbn],
-    queryFn: async () => {
-      const data = await getOneBookData(bookIsbn as string);
-      return data;
-    }
+    queryFn: async () => await getOneBookData(bookIsbn as string),
+    SkeletonComponent: <BookSkeleton />,
+    errorMessage: "도서 상세 페이지를 로드하는 데 실패했습니다."
   });
 
   return (
-    <div className={styles.bookSection}>
-      <div className={styles.title}>
-        <p>{book?.title}</p>
-        <Link
-          to={book?.link as string}
-          className={styles.bookLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          구매하러가기
-        </Link>
-      </div>
-      <img src={book?.image} alt={book?.title} className={styles.image} />
-      <p className={styles.author}>{book?.author} </p>
-      <p
-        className={isExpanded ? `${styles.expanded}` : `${styles.description}`}
-      >
-        {book?.description}
-      </p>
-      <button onClick={handleToggle}>{isExpanded ? "접기" : "더보기"}</button>
-    </div>
+    <>
+      {SkeletonUI ? (
+        SkeletonUI
+      ) : errorMessage ? (
+        <p className={styles.errorText}>{errorMessage}</p>
+      ) : (
+        <div className={styles.bookSection}>
+          <div className={styles.title}>
+            <p>{book?.title}</p>
+            <Link
+              to={book?.link as string}
+              className={styles.bookLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              구매하러가기
+            </Link>
+          </div>
+          <img src={book?.image} alt={book?.title} className={styles.image} />
+          <p className={styles.author}>{book?.author} </p>
+          <p
+            className={
+              isExpanded ? `${styles.expanded}` : `${styles.description}`
+            }
+          >
+            {book?.description}
+          </p>
+          <button onClick={handleToggle}>
+            {isExpanded ? "접기" : "더보기"}
+          </button>
+        </div>
+      )}
+    </>
   );
 };
--- src/pages/detail/components/DiarySection.tsx
+++ src/pages/detail/components/DiarySection.tsx
@@ -1,39 +1,66 @@
-import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import styles from "./diarySection.module.css";
 
 import { getBookWithDiaryPost } from "@/services/apis";
+import { useCustomQueryhook } from "@/pages/home/useCustomQueryHooks";
+
+const DiarySkeleton = () => {
+  return (
+    <>
+      {[1, 2, 3].map((_, index) => (
+        <div key={index} className={styles.diarySkeleton}>
+          <div className={styles.image}></div>
+          <div className={styles.contentBox}>
+            <p className={styles.subTitle}></p>
+            <p className={styles.content}></p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export const DiarySection = ({ bookIsbn }: { bookIsbn: string }) => {
-  const { data: diaries } = useQuery({
+  const {
+    data: diaries,
+    SkeletonUI,
+    errorMessage
+  } = useCustomQueryhook({
     queryKey: ["diaries", bookIsbn],
-    queryFn: async () => {
-      const data = await getBookWithDiaryPost(bookIsbn as string);
-      return data;
-    }
+    queryFn: async () => await getBookWithDiaryPost(bookIsbn as string),
+    SkeletonComponent: <DiarySkeleton />,
+    errorMessage: "다이어리 데이터를 로드하는 데 실패했습니다."
   });
 
   return (
-    <div className={styles.diaryList}>
-      {diaries?.map((diaryData) => {
-        const { diary, post, user } = diaryData;
-        return (
-          <div key={diary.diaryId} className={styles.diary}>
-            <div className={styles.imgBox}>
-              <img src={user.avatar} />
-              <Link to={`/diaries/${diary.diaryId}`}>
-                {user.username}님<br />
-                책장 구경가기
-              </Link>
-            </div>
-            <div className={styles.contentBox}>
-              <h3>{diary.diaryTitle}</h3>
-              <p className={styles.subTitle}>{post.title}</p>
-              <p className={styles.content}>{post.content}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {SkeletonUI ? (
+        <DiarySkeleton />
+      ) : errorMessage ? (
+        <p className={styles.errorText}>{errorMessage}</p>
+      ) : (
+        <div className={styles.diaryList}>
+          {diaries?.map((diaryData) => {
+            const { diary, post, user } = diaryData;
+            return (
+              <div key={diary.diaryId} className={styles.diary}>
+                <div className={styles.imgBox}>
+                  <img src={user.avatar} />
+                  <Link to={`/diaries/${diary.diaryId}`}>
+                    {user.username}님<br />
+                    책장 구경가기
+                  </Link>
+                </div>
+                <div className={styles.contentBox}>
+                  <h3>{diary.diaryTitle}</h3>
+                  <p className={styles.subTitle}>{post.title}</p>
+                  <p className={styles.content}>{post.content}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };

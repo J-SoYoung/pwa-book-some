923741355e0772f32b97cd8390158d2d31d15ec3
thdud <thgml1444@naver.com>
2025-02-18 13:34:28 +0900

    Update [BookSection] : query와 suspense, errorboundary를 사용하여 데이터 상태관리를 함 - useSuspenseQuery를 사용하여 데이터를 가져온다. suspenseQuery는 isLoading / error의 상태관리르 할 필요가 없기 때문에 query객체만 받아온다 - ErrorBoundary와 Suspens적용을 위해서 qeury데이터와 관련된 UI는 따로 컴포넌트를 분리했다. - ErrorBoundary > Suspense > Query 순으로 UI를 렌더링 할 수 있도록 하였다 => 나중에 컴포넌트화 할 수 있을 듯.

--- src/pages/detail/components/BookSection.tsx
+++ src/pages/detail/components/BookSection.tsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./bookSection.module.css";
+import {
+  useQueryErrorResetBoundary,
+  useSuspenseQuery
+} from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
+import styles from "./bookSection.module.css";
 import { getOneBookData } from "@/services/apis";
-import { useQuery } from "@tanstack/react-query";
-import { QueryResultHandler } from "@/components/QueryResultHandler";
+import { ErrorFallback } from "@/components";
 
 const BookSkeleton = () => {
   return (
     <div className={styles.bookSkeleton}>
-      <div className={styles.title}></div>
+      <div className={styles.title} />
       <div className={styles.image} />
-      <p className={styles.author}></p>
-      <p className={styles.description}></p>
+      <p className={styles.author} />
+      <p className={styles.description} />
     </div>
   );
 };
 
-export const BookSection = ({ bookIsbn }: { bookIsbn: string }) => {
+export const BookContent = ({ bookIsbn }: { bookIsbn: string }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const handleToggle = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const {
-    data: book,
-    isLoading,
-    isError
-  } = useQuery({
+  const { data: book } = useSuspenseQuery({
     queryKey: ["book", bookIsbn],
     queryFn: async () => {
       const data = await getOneBookData(bookIsbn as string);
@@ -36,35 +36,38 @@ export const BookSection = ({ bookIsbn }: { bookIsbn: string }) => {
   });
 
   return (
-    <QueryResultHandler
-      isLoading={isLoading}
-      isError={isError}
-      errorMessage="도서 상세 페이지를 로드하는 데 실패했습니다."
-      SkeletonComponent={<BookSkeleton />}
-    >
-      <div className={styles.bookSection}>
-        <div className={styles.title}>
-          <p>{book?.title}</p>
-          <Link
-            to={book?.link as string}
-            className={styles.bookLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            구매하러가기
-          </Link>
-        </div>
-        <img src={book?.image} alt={book?.title} className={styles.image} />
-        <p className={styles.author}>{book?.author} </p>
-        <p
-          className={
-            isExpanded ? `${styles.expanded}` : `${styles.description}`
-          }
+    <div className={styles.bookSection}>
+      <div className={styles.title}>
+        <p>{book?.title}</p>
+        <Link
+          to={book?.link as string}
+          className={styles.bookLink}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {book?.description}
-        </p>
-        <button onClick={handleToggle}>{isExpanded ? "접기" : "더보기"}</button>
+          구매하러가기
+        </Link>
       </div>
-    </QueryResultHandler>
+      <img src={book?.image} alt={book?.title} className={styles.image} />
+      <p className={styles.author}>{book?.author} </p>
+      <p
+        className={isExpanded ? `${styles.expanded}` : `${styles.description}`}
+      >
+        {book?.description}
+      </p>
+      <button onClick={handleToggle}>{isExpanded ? "접기" : "더보기"}</button>
+    </div>
+  );
+};
+
+export const BookSection = ({ bookIsbn }: { bookIsbn: string }) => {
+  const { reset } = useQueryErrorResetBoundary();
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+      <Suspense fallback={<BookSkeleton />}>
+        <BookContent bookIsbn={bookIsbn} />
+      </Suspense>
+    </ErrorBoundary>
   );
 };

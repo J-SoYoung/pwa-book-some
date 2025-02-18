8e371ea26dcd94fc6e8f08bbb55b414d15e4eca4
thdud <thgml1444@naver.com>
2025-02-18 13:38:48 +0900

    Refactor [DiarySection] : useQuery + suspense + errorBoundary 를 사용하여 UI를 구성 - useQuery 대신 useSuspenseQuery로 데이터를 가져옴, suspenseQuery는 react와 연동하여 상태관리 데이터를 받아오지 않아도됨 - query 데이터만 받아와 UI렌더링함 -> 상태관리는 suspense와 errorboundary가 함

--- src/pages/detail/components/DiarySection.tsx
+++ src/pages/detail/components/DiarySection.tsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 import styles from "./diarySection.module.css";
 
 import { getBookWithDiaryPost } from "@/services/apis";
-import { useCustomQueryhook } from "@/pages/home/useCustomQueryHooks";
+import { useQueryErrorResetBoundary, useSuspenseQuery } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "@/components";
+import { Suspense } from "react";
 
 const DiarySkeleton = () => {
   return (
@@ -20,47 +23,51 @@ const DiarySkeleton = () => {
   );
 };
 
-export const DiarySection = ({ bookIsbn }: { bookIsbn: string }) => {
-  const {
-    data: diaries,
-    SkeletonUI,
-    errorMessage
-  } = useCustomQueryhook({
+export const DiaryContent = ({ bookIsbn }: { bookIsbn: string }) => {
+  const { data: diaries } = useSuspenseQuery({
     queryKey: ["diaries", bookIsbn],
-    queryFn: async () => await getBookWithDiaryPost(bookIsbn as string),
-    SkeletonComponent: <DiarySkeleton />,
-    errorMessage: "다이어리 데이터를 로드하는 데 실패했습니다."
+    queryFn: async () => {
+      const data = await getBookWithDiaryPost(bookIsbn as string);
+      return data;
+    }
   });
 
   return (
     <>
-      {SkeletonUI ? (
-        <DiarySkeleton />
-      ) : errorMessage ? (
-        <p className={styles.errorText}>{errorMessage}</p>
-      ) : (
-        <div className={styles.diaryList}>
-          {diaries?.map((diaryData) => {
-            const { diary, post, user } = diaryData;
-            return (
-              <div key={diary.diaryId} className={styles.diary}>
-                <div className={styles.imgBox}>
-                  <img src={user.avatar} />
-                  <Link to={`/diaries/${diary.diaryId}`}>
-                    {user.username}님<br />
-                    책장 구경가기
-                  </Link>
-                </div>
-                <div className={styles.contentBox}>
-                  <h3>{diary.diaryTitle}</h3>
-                  <p className={styles.subTitle}>{post.title}</p>
-                  <p className={styles.content}>{post.content}</p>
-                </div>
+      <div className={styles.diaryList}>
+        {diaries?.map((diaryData) => {
+          const { diary, post, user } = diaryData;
+          return (
+            <div key={diary.diaryId} className={styles.diary}>
+              <div className={styles.imgBox}>
+                <img src={user.avatar} />
+                <Link to={`/diaries/${diary.diaryId}`}>
+                  {user.username}님<br />
+                  책장 구경가기
+                </Link>
               </div>
-            );
-          })}
-        </div>
-      )}
+              <div className={styles.contentBox}>
+                <h3>{diary.diaryTitle}</h3>
+                <p className={styles.subTitle}>{post.title}</p>
+                <p className={styles.content}>{post.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
+  );
+};
+
+
+export const DiarySection = ({ bookIsbn }: { bookIsbn: string }) => {
+  const { reset } = useQueryErrorResetBoundary();
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+      <Suspense fallback={<DiarySkeleton />}>
+        <DiaryContent bookIsbn={bookIsbn} />
+      </Suspense>
+    </ErrorBoundary>
   );
 };

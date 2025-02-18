950db5a881b84f2eea40389574bcc7352067aa2b
thdud <thgml1444@naver.com>
2025-02-18 13:29:31 +0900

    Refactor : Hook 등 다양한 라이브러리를 적용하기 위해 main.tsx의 내용을App components로 정의함

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import Router from "./routers/index.tsx";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Router />
-      </QueryClientProvider>
-    </RecoilRoot>
-  </React.StrictMode>
-);
+export const App = () => {
+  return (
+    <React.StrictMode>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

00cef3782fb51d12bf7a11b53ee7bc7702e00d75
thdud <thgml1444@naver.com>
2025-02-21 19:40:54 +0900

    Refactor [ Home ] : Home에서는 각 컴포넌트의 상태에 따라 Suspense와 ErrorBoundary를 적용하도록 책임분리 - 이전 : 추천책의 데이터를 불러와 상태 및 데이터 UI렌더링을 관리하였음 - 리팩토링 : Home 하위의 컴포넌트의 상태에 따라 Suspense와 ErrorBoundary가 상태UI를 렌더링하도록 함

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,59 +1,24 @@
-import styles from "./home.module.css";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 
-import { Items } from "@/components/items/Items";
-import { getRecommendBooks } from "@/services/apis";
-import { useEffect } from "react";
-import { useRecoilValue } from "recoil";
-import { userState } from "@/recoil/atoms";
-import { BookType, UserType } from "@/services/types/dataTypes";
-import { ItemSkeleton } from "@/components/items/ItemSkeleton";
-import UserBooks from "./components/UserBooks";
-import { useCustomQueryhook } from "./useCustomQueryHooks";
+import { UserBookSkeleton, UserBooks, HomeContents } from "./components";
+import { ItemsSkeleton } from "@/components/items";
+import { ErrorFallback } from "@/components";
 
 export const Home = () => {
-  const user = useRecoilValue(userState) as UserType;
-  useEffect(() => {
-    if (user.userId === "") {
-      alert("로그인이 필요합니다.");
-      window.location.href = "/login";
-    }
-  });
-
-  const {
-    data: recommendBooks,
-    SkeletonUI,
-    errorMessage
-  } = useCustomQueryhook({
-    queryKey: ["recommendedBooks"],
-    queryFn: getRecommendBooks,
-    SkeletonComponent: <ItemSkeleton />,
-    errorMessage: "추천 도서를 불러오는데 실패했습니다."
-  });
+  const { reset } = useQueryErrorResetBoundary();
 
   return (
-    <main className={styles.home}>
-      <section className={styles.section}>
-        <h3>이 책을 추천합니다</h3>
-        <div className={styles.itemListBox}>
-          {SkeletonUI ? (
-            SkeletonUI
-          ) : errorMessage ? (
-            <p className={styles.errorText}>{errorMessage}</p>
-          ) : (
-            recommendBooks?.map((book: BookType) => {
-              const data = {
-                url: `/detail/${book.isbn}`,
-                imageUrl: book.image,
-                title: book.title
-              };
-              return <Items data={data} key={book.isbn} />;
-            })
-          )}
-        </div>
-      </section>
-
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+      <h3>이 책을 추천합니다</h3>
+      <Suspense fallback={<ItemsSkeleton />}>
+        <HomeContents />
+      </Suspense>
       <h3>다른 유저들은 이런 책을 읽고 있어요!</h3>
-      <UserBooks />
-    </main>
+      <Suspense fallback={<UserBookSkeleton />}>
+        <UserBooks />
+      </Suspense>
+    </ErrorBoundary>
   );
 };

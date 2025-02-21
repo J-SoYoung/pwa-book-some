43fdf3388912d234b5687bb4fcdca3eca59a5b91
thdud <thgml1444@naver.com>
2025-02-21 19:38:59 +0900

    Add [ index ] : 폴더의 파일들을 폴더명으로 export 하기 위해 index의 각 파일을 모아서 처리함

---
+++ src/pages/home/components/index.ts
@@ -0,0 +1,5 @@
+import { UserBookSkeleton } from "./UserBookSkeleton";
+import { UserBooks } from "./UserBooks";
+import { HomeContents } from "./HomeContents";
+
+export { UserBookSkeleton, UserBooks, HomeContents };

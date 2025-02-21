1d7ab9977e99a326cab2bb44884f8d72c4f58499
thdud <thgml1444@naver.com>
2025-02-21 18:27:40 +0900

    Add [DiarySkeleton] : 부분/전체 스켈레돈 UI생성

--- src/pages/diaries/components/Skeleton.tsx
+++ src/pages/diaries/components/Skeleton.tsx
@@ -1,20 +1,32 @@
 import styles from "./skeleton.module.css";
 
-export const Skeleton = () => {
+export const DiaryItemSkeleton = () => {
   return (
-    <div>
+    <>
       <div className={styles.title} />
       <div className={styles.diaryBackground}>
         <div className={styles.image} />
       </div>
-      <div className={styles.postSkeleton}>
-        {[1, 2, 3].map((_, index) => (
-          <div key={index} className={styles.contentBox}>
-            <p></p>
-            <p></p>
-          </div>
-        ))}
-      </div>
+    </>
+  );
+};
+export const PostListsSkeleton = () => {
+  return (
+    <div className={styles.postSkeleton}>
+      {[1, 2, 3].map((_, index) => (
+        <div key={index} className={styles.contentBox}>
+          <p></p>
+          <p></p>
+        </div>
+      ))}
     </div>
+  );
+};
+export const Skeleton = () => {
+  return (
+    <>
+      <DiaryItemSkeleton />
+      <PostListsSkeleton />
+    </>
   );
 };

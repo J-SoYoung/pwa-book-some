0a460dbad9f21b41946eca4a3614483064824323
thdud <thgml1444@naver.com>
2025-02-21 20:10:21 +0900

    Rename : 폴더경로 변경 및 파일명 변경으로 인한 파일 삭제 및 생성

--- src/pages/my-book/components/ItemLists.tsx
+++ src/pages/my-book/components/ItemLists.tsx
@@ -1,21 +1,18 @@
 import { DiariesType } from "@/services/types/dataTypes";
 import { Items } from "@/components/items/Items";
-import styles from "./mybook.module.css";
+import styles from "./itemLists.module.css";
 
 interface ItemListsProps {
-  sectionTitle: string;
   diaries: DiariesType[];
   noDataText: string;
 }
 
 export const ItemLists = ({
-  sectionTitle,
   diaries,
   noDataText
 }: ItemListsProps) => {
   return (
     <section className={styles.section}>
-      <h3>{sectionTitle}</h3>
       <div className={styles.itemListsBox}>
         {diaries.length > 0 ? (
           <>

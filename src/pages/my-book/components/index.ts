53ce8e7dc2981f8cc3a17e929ee67eea0b4f42ad
thdud <thgml1444@naver.com>
2025-02-21 20:20:51 +0900

    Chore : 컴포넌트 폴더가 생성됨에 따라 같은 경로로 파일을 한번에 export하기위해 index생성

---
+++ src/pages/my-book/components/index.ts
@@ -0,0 +1,5 @@
+import { ItemLists } from "./ItemLists";
+import { LikeDiariesContents } from "./LikeDiariesContents";
+import { ReadingBookContents } from "./ReadingBookContents";
+
+export { ItemLists, LikeDiariesContents, ReadingBookContents };

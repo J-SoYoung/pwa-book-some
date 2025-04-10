9ad6f64d7034c5c438c600945b78b8085c1cb9d2
thdud <thgml1444@naver.com>
2025-04-10 12:17:38 +0900

    Chore : 말줄임표 삭제

--- src/shared/utils/textFromHtml.ts
+++ src/shared/utils/textFromHtml.ts
@@ -1,7 +1,7 @@
 export const textFromHtml = (html: string) => {
   const tmp = document.createElement("div");
   tmp.innerHTML = html;
-  const text = tmp.innerText;
+  const text = tmp.textContent || tmp.innerText;
   return text;
   // return text.length > length ? text.slice(0, length) + "..." : text;
 };

dc133af6cfb90e0a1a5e73fd3ddb2e2c2c12fa31
thdud <thgml1444@naver.com>
2025-04-16 14:50:31 +0900

    [REMOVE] 사용하지 않는 파일 삭제

--- src/shared/components/layout/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { BottomNav } from "./BottomNav";
-import { Header } from "./Header";
-import { SearchBar } from "./SearchBar";
-import { ScrollReset } from "./ScrollReset";
-import { ScrollTopButton } from "./ScrollTopButton";
-
-export { BottomNav, Header, SearchBar, ScrollReset, ScrollTopButton };

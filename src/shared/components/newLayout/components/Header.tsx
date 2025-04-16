a583b8abf769b6a39b6be52b1edd5cc24d1bcb9a
thdud <thgml1444@naver.com>
2025-04-16 16:00:43 +0900

    [UPDATE-header] header에서 글쓰기 버튼을 하단 네비게이션과 동일한 기능으로 수정 - popup-button생성 포스트 작성 페이지 이동 하도록 / 로그인한 유저가 아니면 로그인 페이지로 갈 수 있도록 수정

--- src/shared/components/newLayout/components/Header.tsx
+++ src/shared/components/newLayout/components/Header.tsx
@@ -1,13 +1,31 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import styles from "../styles/header.module.css";
 
 import { UserType } from "@/shared/types/dataTypes";
 import { userState } from "@/shared/recoil/atoms";
+import PopupButton from "../../PopupButton";
 
 export const Header = () => {
+  const navigate = useNavigate();
   const user = useRecoilValue<UserType | null>(userState);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const unKnownUser = () => {
+    if (confirm("로그인이 필요합니다. 페이지를 이동하시겠습니까?")) {
+      navigate("/login");
+    }
+  };
+
+  const togglePopup = () => {
+    if (!user) {
+      unKnownUser();
+    } else {
+      setShowPopup(!showPopup);
+    }
+  };
 
   return (
     <header className={styles.header}>
@@ -15,16 +33,17 @@ export const Header = () => {
         <Link to="/home">BOOKSOME</Link>
       </h1>
       <nav className={styles.right}>
-        <Link to={`/mypage/${user?.userId}`} className={styles.username}>
+        <Link to={`/mypage/${user?.userId}`}>
           {user ? `${user.username}님` : "환영합니다"}
         </Link>
         <Link to={`/mybook/${user?.userId}`} className={styles.button}>
           내서재
         </Link>
-        <Link to={`/postsNew`} className={styles.button}>
+        <button onClick={togglePopup} className={styles.button}>
           글쓰기
-        </Link>
+        </button>
       </nav>
+      {showPopup && <PopupButton onClose={togglePopup} />}
     </header>
   );
 };

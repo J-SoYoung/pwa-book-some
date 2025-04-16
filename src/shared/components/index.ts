783c3967526316aa72f21bc708811d7525b652e1
thdud <thgml1444@naver.com>
2025-04-16 14:49:47 +0900

    [RENAME] 반응형 Layout컴포넌트 생성에 따른 하위 컴포넌트 파일 이동 및 수정 - scrollReset은 컴포넌트가 아닌 hooks으로 생성하여 layout컴포넌트에서 실행됨 - 나머지는 파일 이동

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,11 +1,11 @@
 import { Items } from "./items/Items";
 import { ItemsSkeleton } from "./items/ItemsSkeleton";
 import { InputField, InputEditField, TextareaField } from "./InputField";
-
+import { PopupButton } from "./PopupButton";
+import { ScrollTopButton } from './ScrollTopButton';
 import { ErrorFallback } from "./errorComp/ErrorFallback";
 import { WrapperSuspense } from "./errorComp/WrapperSuspense";
 import { LoadingSpinner } from "./LoadingSpinner";
-
 
 export {
   Items,
@@ -16,4 +16,6 @@ export {
   ErrorFallback,
   WrapperSuspense,
   LoadingSpinner,
+  PopupButton,
+  ScrollTopButton
 };
---
+++ src/shared/components/newLayout/hooks/useScrollReset.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const useScrollReset = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지 이동 시 스크롤 맨 위로
+  }, [pathname]);
+
+  return null;
+};

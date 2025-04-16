7debf2b334b3d562e2cdbf89036f08c14a53e461
thdud <thgml1444@naver.com>
2025-04-16 15:56:12 +0900

    [Style-recoBook] 사용하지 않는 태그 삭제, book 정렬을 flex가 아닌 grid로 수정

--- src/pages/home/components/RecommededBooks.tsx
+++ src/pages/home/components/RecommededBooks.tsx
@@ -13,19 +13,15 @@ export const RecommededBooks = () => {
   });
 
   return (
-    <main className={styles.home}>
-      <section className={styles.section}>
-        <div className={styles.itemListBox}>
-          {recommendBooks?.map((book: BookType, idx) => {
-            const data = {
-              url: `/detail/${book.isbn}`,
-              imageUrl: book.image,
-              title: book.title
-            };
-            return <Items data={data} key={idx} />;
-          })}
-        </div>
-      </section>
-    </main>
+    <section className={styles.itemListBox}>
+      {recommendBooks?.map((book: BookType, idx) => {
+        const data = {
+          url: `/detail/${book.isbn}`,
+          imageUrl: book.image,
+          title: book.title
+        };
+        return <Items data={data} key={idx} />;
+      })}
+    </section>
   );
 };

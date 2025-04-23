f20c31301f3dbfc453c0d68d834824a35a5f908e
thdud <thgml1444@naver.com>
2025-04-23 14:08:10 +0900

    [Update-Home] suspense title을 삭제하고 각 컴포넌트 내부에 h3의 타이틀 추가하는 것으로 변경, suspense는 skeleton을 렌더링 하는것으로함

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,12 @@
 import { UserBookSkeleton, UserBooks, RecommededBooks } from "./index";
-import { WrapperSuspense,ItemsSkeleton } from "@/shared/components";
+import { WrapperSuspense } from "@/shared/components";
 
 export const Home = () => {
   return (
     <main>
-      <WrapperSuspense title="이 책을 추천합니다" fallback={<ItemsSkeleton />}>
-        <RecommededBooks />
-      </WrapperSuspense>
-      <WrapperSuspense
-        title="다른 유저들은 이런 책을 읽고 있어요!"
-        fallback={<UserBookSkeleton />}
-      >
-        <UserBooks />
+      <WrapperSuspense fallback={<UserBookSkeleton />}>
+        <RecommededBooks title="이 책을 추천합니다" />
+        <UserBooks title="다른 유저들은 이런 책을 읽고 있어요!" />
       </WrapperSuspense>
     </main>
   );

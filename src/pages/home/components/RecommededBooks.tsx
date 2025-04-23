9f7bc1969365a4144163c46f9c49891bad856f2f
thdud <thgml1444@naver.com>
2025-04-23 14:09:22 +0900

    [Update-Home컴포넌트들] suspense에서 렌더링 하던 title을 각 컴포넌트에서 렌더링하도록 수정

--- src/pages/home/components/RecommededBooks.tsx
+++ src/pages/home/components/RecommededBooks.tsx
@@ -6,22 +6,25 @@ import { getRecommendBooks } from "../service/getFirebaseData";
 import { Items } from "@/shared/components";
 import { BookType } from "@/shared/types/dataTypes";
 
-export const RecommededBooks = () => {
+export const RecommededBooks = ({ title }: { title: string }) => {
   const { data: recommendBooks } = useSuspenseQuery({
     queryKey: ["recommendedBooks"],
     queryFn: getRecommendBooks
   });
 
   return (
-    <section className={styles.itemListBox}>
-      {recommendBooks?.map((book: BookType, idx) => {
-        const data = {
-          url: `/detail/${book.isbn}`,
-          imageUrl: book.image,
-          title: book.title
-        };
-        return <Items data={data} key={idx} />;
-      })}
-    </section>
+    <>
+      {title && <h3>{title}</h3>}
+      <section className={styles.itemListBox}>
+        {recommendBooks?.map((book: BookType) => {
+          const data = {
+            url: `/detail/${book.isbn}`,
+            imageUrl: book.image,
+            title: book.title
+          };
+          return <Items data={data} key={book.isbn} />;
+        })}
+      </section>
+    </>
   );
 };
--- src/pages/home/components/UserBooks.tsx
+++ src/pages/home/components/UserBooks.tsx
@@ -7,7 +7,7 @@ import { getAllkDiaries } from "../service/getFirebaseData";
 import { LikeButton } from "./LikeButton";
 import { textFromHtml } from "@/shared/utils";
 
-export const UserBooks = () => {
+export const UserBooks = ({ title }: { title: string }) => {
   const navigate = useNavigate();
 
   const { data: allDiaries } = useSuspenseQuery({
@@ -16,40 +16,43 @@ export const UserBooks = () => {
   });
 
   return (
-    <section className={styles.userBooks}>
-      {allDiaries?.map((diaryData) => {
-        const { book, diary, post, user } = diaryData;
-        return (
-          <article className={styles.bookCard} key={diary.diaryId}>
-            <div className={styles.imageBox}>
-              <img
-                className={styles.bookImage}
-                src={book.image}
-                alt={book.title}
-              />
-              <img
-                className={styles.userAvatar}
-                src={user.avatar}
-                alt={`${user.username}의 다이어리`}
-              />
-            </div>
-            <div className={styles.likeBox}>
-              <LikeButton diaryId={diary.diaryId} />
-            </div>
+    <>
+      {title && <h3>{title}</h3>}
+      <section className={styles.userBooks}>
+        {allDiaries?.map((diaryData) => {
+          const { book, diary, post, user } = diaryData;
+          return (
+            <article className={styles.bookCard} key={diary.diaryId}>
+              <div className={styles.imageBox}>
+                <img
+                  className={styles.bookImage}
+                  src={book.image}
+                  alt={book.title}
+                />
+                <img
+                  className={styles.userAvatar}
+                  src={user.avatar}
+                  alt={`${user.username}의 다이어리`}
+                />
+              </div>
+              <div className={styles.likeBox}>
+                <LikeButton diaryId={diary.diaryId} />
+              </div>
 
-            <div
-              className={styles.bookInfo}
-              onClick={() => {
-                navigate(`/diaries/${diary.diaryId}`);
-              }}
-            >
-              <h3>{diary.diaryTitle.slice(0,17)}</h3>
-              <p>{post.title}</p>
-              <p>{textFromHtml(post.content)}</p>
-            </div>
-          </article>
-        );
-      })}
-    </section>
+              <div
+                className={styles.bookInfo}
+                onClick={() => {
+                  navigate(`/diaries/${diary.diaryId}`);
+                }}
+              >
+                <h3>{diary.diaryTitle.slice(0, 17)}</h3>
+                <p>{post.title}</p>
+                <p>{textFromHtml(post.content)}</p>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </>
   );
 };
